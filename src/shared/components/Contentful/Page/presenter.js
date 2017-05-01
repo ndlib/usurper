567fdb22bb6fe11385661518e1c14380009d9b73
Harrison Beachey <hbeachey@nd.edu>
2017-05-01 16:06:13 -0400

    make sure link still works

--- src/shared/components/Contentful/Page/presenter.js
+++ src/shared/components/Contentful/Page/presenter.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import './style.css'
+import { Link } from 'react-router-dom'
 import LibMarkdown from '../../LibMarkdown'
 
 const Loading = (
