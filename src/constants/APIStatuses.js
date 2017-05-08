dc09537921ff343658c315f29c9eb159b4a3f257
Justin Gondron <jgondron@nd.edu>
2017-05-08 11:33:21 -0400

    WR-338: Errors/not found/status handling - Added comment to describe intended use. Not sure if this should be called APIStatuses or AsyncStatuses

--- src/constants/APIStatuses.js
+++ src/constants/APIStatuses.js
@@ -1,3 +1,5 @@
+// Intended to be used as state/status for any components
+// that perform async requests to some API or data store
 export const FETCHING = 'API_STATUS_FETCHING'
 export const SUCCESS = 'API_STATUS_SUCCESS'
 export const NOT_FOUND = 'API_STATUS_NOT_FOUND'
