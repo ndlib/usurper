14b6d8af66d1038ee7a339b75001ca1016949b44
Justin Gondron <jgondron@nd.edu>
2017-05-11 15:51:12 -0400

    WR-340: Presenter factory - Moved a lot of reusable behavior for rendering the correct presenter based on api status into a factory object. - Changed ContentfulPageContainer to use componentDidMount for calling actions. Usage within mapDispatchToProps is not likely the best place to do this.

--- src/components/Contentful/Page/presenter.js
+++ src/components/Contentful/Page/presenter.js
@@ -7,12 +7,8 @@ import LibMarkdown from '../../LibMarkdown'
 import Related from '../../Related'
 import Image from '../../Image'
 import Librarians from '../../Librarians'
-import * as statuses from '../../../constants/APIStatuses'
-import NotFound from '../../Messages/NotFound'
-import Loading from '../../Messages/Loading'
-import ErrorMessage from '../../Messages/Error'
 
-const Page = (cfPageEntry) => (
+const PagePresenter = (cfPageEntry) => (
   <div className='container-fluid'>
     <h2>{ cfPageEntry.fields.title }</h2>
     <hr />
@@ -32,21 +28,8 @@ const Page = (cfPageEntry) => (
   </div>
 )
 
-const Presenter = ({ cfPageEntry }) => {
-  switch(cfPageEntry.status){
-    case statuses.FETCHING:
-      return <Loading/>
-    case statuses.SUCCESS:
-      return Page(cfPageEntry.json)
-    case statuses.NOT_FOUND:
-      return <NotFound/>
-    default:
-      return <ErrorMessage message={ 'There was an error loading the page.' }/>
-  }
+PagePresenter.propTypes = {
+  cfPageEntry: PropTypes.object.isRequired
 }
 
-Presenter.propTypes = {
-  cfPageEntry: PropTypes.object.isRequired,
-}
-
-export default Presenter
+export default PagePresenter
