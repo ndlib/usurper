a3bc66e1eb623f7f6c08dd26a6267e9049cd614d
Justin Gondron <jgondron@nd.edu>
2017-05-16 11:26:18 -0400

    WR-340-Did-mount-actions - Refactored librarians container component to use componentDidMount for its initial dispatch

--- src/components/Librarians/index.js
+++ src/components/Librarians/index.js
@@ -1,21 +1,33 @@
-// Container component for a Page content type from Contentful
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import { fetchLibrarians } from '../../actions/librarians'
-import React from 'react'
+import React, { Component } from 'react'
 import LibrarianPresenter from './presenter.js'
 
 const mapStateToProps = (state, ownProps) => {
-  return { librarianInfo: state.librarianInfo }
+  return {
+    librarianInfo: state.librarianInfo,
+    ...ownProps,
+  }
 }
 
 const mapDispatchToProps = (dispatch, ownProps) => {
-  dispatch(fetchLibrarians(ownProps.netids))
-  return {}
+  return bindActionCreators({ fetchLibrarians }, dispatch)
+}
+
+export class LibrariansContainer extends Component {
+  componentDidMount(){
+    this.props.fetchLibrarians(this.props.netids)
+  }
+
+  render () {
+    return <LibrarianPresenter {...this.props} />
+  }
 }
 
 const Librarians = connect(
   mapStateToProps,
   mapDispatchToProps
-)(LibrarianPresenter)
+)(LibrariansContainer)
 
 export default Librarians
