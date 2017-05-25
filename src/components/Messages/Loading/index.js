3ee7c9800d215c1140a2572817bc8ac2a6c2a628
dan <danthewolfe@users.noreply.github.com>
2017-05-25 13:11:42 -0400

    revert

--- src/components/Messages/Loading/index.js
+++ src/components/Messages/Loading/index.js
@@ -1,10 +1,8 @@
 import { connect } from 'react-redux'
 import React from 'react'
-import PageTitle from '../../PageTitle'
 
 const Loading = ({ message = '' }) => (
   <div className={'Loading'}>
-    <PageTitle title='...' />
     <h1>Loading</h1>
     <div>{ message }</div>
   </div>
