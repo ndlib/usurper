9a151ef02998aad4c49bddee4f9129abbdecb593
dan <danthewolfe@users.noreply.github.com>
2017-05-25 13:11:19 -0400

    revert

--- src/components/Messages/Error/index.js
+++ src/components/Messages/Error/index.js
@@ -1,10 +1,8 @@
 import { connect } from 'react-redux'
 import React from 'react'
-import PageTitle from '../../PageTitle'
 
 const Error = ({ message = 'An error has occured' }) => (
   <div className={'Error'}>
-    <PageTitle title='An Error Occurred' />
     <h1>Error</h1>
     <div>{ message }</div>
   </div>
