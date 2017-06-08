5ea5ee59a90b4ed48f4f42d44e151be34f0a3855
Andy Wetherill <awetheri@nd.edu>
2017-06-08 10:24:09 -0400

    Home page & search appliance cleanup.
    # 50-character subject line
    #
    # 72-character wrapped longer description. This should answer:
    #
    # * Why was this change necessary?
    # * How does it address the problem?
    # * Are there any side effects?
    #
    # Include a link to the ticket, if any.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -4,6 +4,9 @@ import '../../static/css/global.css'
 import HomePageHours from '../Hours/HomePage'
 import HeaderHours from '../Hours/Header'
 import PageTitle from '../PageTitle'
+import Reserves from '../../static/images/reserves.jpg'
+import Ill from '../../static/images/ill.jpg'
+import Room from '../../static/images/reserveroom.jpg'
 
 
 class Home extends Component {
@@ -14,6 +17,17 @@ class Home extends Component {
         <PageTitle tile='Hesburgh Library' />
         <HomePageHours jsonHoursApiKey='hesburghlibrariesservicepoints' />
         <HeaderHours jsonHoursApiKey='hesburghlibrariesservicepoints' />
+        <div className='row services'>
+			<div className='col-sm-4 col-xs-4'>
+				<a href='https://reserves.library.nd.edu" title="Course Reserves'><img src={Reserves} alt='Course Reserves. A row of textbooks.' /></a>
+			</div>
+			<div className='col-sm-4 col-xs-4'>
+				<a href='https://nd.illiad.oclc.org/illiad/IND/illiad.dll' title='Interlibrary Loan and Document Delivery'><img src={Ill} alt='Interlibrary Loan and Document Delivery.  A graphical representation of two libraries exchanging infromation.' /></a>
+			</div>
+			<div className='col-sm-4 col-xs-4'>
+				<a href='http://nd.libcal.com/#s-lc-box-2749-container-tab1' title='Reserve a Room'><img src={Room} alt='Reserve a Room. Students sitting at a table receiving instruction.' /></a>
+			</div>
+    	</div>
       </div>
     )
   }
