976ec9de68ba84f621a0545cd350e34f9faaa1b7
Dan Wolfe <dwolfe2@nd.edu>
2017-06-14 10:30:43 -0400

    Add Services and Libraries pages and stub for About

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -13,6 +13,9 @@ import ContentfulPage from '../../components/Contentful/Page'
 import SecureContentfulPage from '../../components/Contentful/SecurePage'
 import ContentfulFloor from '../../components/Contentful/Floor'
 import Research from '../../components/LandingPages/Research'
+import Services from '../../components/LandingPages/Services'
+import Libraries from '../../components/LandingPages/Libraries'
+import About from '../../components/LandingPages/About'
 import rootReducers from '../../reducers'
 import thunkMiddleware from 'redux-thunk'
 
@@ -30,13 +33,16 @@ const App = () => {
       <PageWrapper>
         <Switch>
           <Route exact path='/' component={Home} />
+          <Route exact path='/about' component={About} />
           <Route exact path='/chat' component={Chat} />
           <Route exact path='/courses' component={Courses} />
           <Route exact path='/hours' component={Hours} />
           <Route exact path='/floor/:id' component={ContentfulFloor} />
+          <Route exact path='/libraries' component={Libraries} />
           <Route exact path='/personal' component={PersonalInfo} />
           <Route exact path='/research' component={Research} />
           <Route exact path='/secure/:id' component={SecureContentfulPage} />
+          <Route exact path='/services' component={Services} />
           <Route exact path='/:id' component={ContentfulPage} />
         </Switch>
       </PageWrapper>
---
+++ src/components/LandingPages/Services/index.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import Link from '../../Link'
+import ReserveRoom from '../../../static/images/reserveroom.jpg'
+import PageTitle from '../../PageTitle'
+const Services = () => {
+  return (
+    <div >
+      <h1>All Services</h1>
+      <hr />
+      <p>A description about the services</p>
+
+      <div className='row'>
+        <div className='col-md-4 col-xs-12'>
+          <h3>Featured Services</h3>
+
+          <p><Link to='/room-reservations/'><img src={ReserveRoom} /></Link></p>
+
+        </div>
+        <div className='col-md-4 col-md-offset-1 col-xs-12'>
+          <h3>Borrowing</h3>
+
+          <p><Link to='/borrow-renew-request/'>Borrow, Renew, Request Policies</Link> </p>
+
+          <p><Link to='/technology-lending/'>Technology Lending Policies</Link> </p>
+
+          <p>&nbsp;</p>
+
+          <h3>Teaching and Consulting</h3>
+
+          <p><Link to='/request-library-research-instruction/'>Request Library Research Instruction</Link> </p>
+
+          <p><Link to='/workshops/'>Workshops</Link> </p>
+
+          <p><Link to='/thesis-dissertation-camps/'>Thesis and Dissertation Camps</Link> </p>
+
+          <p><Link to='/consultation-teaching/'>Consultations (Meet with a Librarian)</Link> </p>
+
+        </div>
+      </div>
+      <PageTitle title='Services' />
+    </div>
+  )
+}
+
+export default Services
