3584e36d69de52cc689472db04c53827f05e1fe4
Harrison Beachey <hbeachey@nd.edu>
2017-06-16 11:29:21 -0400

    add subject list page

---
+++ src/components/SubjectList/index.js
@@ -0,0 +1,53 @@
+// Container component for a Page content type from Contentful
+import React from 'react'
+
+const SubjectList = () => {
+  return (
+    <div className='container-fluid row'>
+      <div className='col-md-6'>
+        <p><a href='/architecture'>Architecture</a></p>
+        <p><a href='/africana-studies'>Africana Studies</a></p>
+        <p><a href='/american-studies'>American Studies</a></p>
+        <p><a href='/mathematics'>Applied Computational Mathematics &amp; Statistics</a></p>
+        <p><a href='/anthropology'>Anthropology</a></p>
+        <p><a href='/art-art-history-design'>Art, Art History, &amp; Design</a></p>
+        <p><a href='/biology'>Biology, Environmental, &amp; Life Sciences</a></p>
+        <p><a href='/business'>Business</a></p>
+        <p><a href='/byzantine-studies'>Byzantine Studies</a></p>
+        <p><a href='/catholic-studies'>Catholic Studies</a></p>
+        <p><a href='/chemistry'>Chemistry &amp; Biochemistry</a></p>
+        <p><a href='/classics'>Classics</a></p>
+        <p><a href='/east-asian-studies'>East Asian Language, Literature, &amp; Culture</a></p>
+        <p><a href='/economics'>Economics</a></p>
+        <p><a href='/education'>Education</a></p>
+        <p><a href='/engineering'>Engineering</a></p>
+        <p><a href='/english'>English Language &amp; Literature</a></p>
+        <p><a href='/film-television-theatre'>Film, Television, &amp; Theatre</a></p>
+        <p><a href='/french'>French Language, Literature, &amp; Culture</a></p>
+      </div>
+      <div className='col-md-6'>
+        <p><a href='/gender-studies'>Gender Studies</a></p>
+        <p><a href='/german'>German Language, Literature, &amp; Culture</a></p>
+        <p><a href='/global-affairs'>Global Affairs</a></p>
+        <p><a href='/history'>History</a></p>
+        <p><a href='/history-and-philosophy-of-science'>History and Philosophy of Science</a></p>
+        <p><a href='/irish-studies'>Irish Studies</a></p>
+        <p><a href='/italian'>Italian Language, Literature, &amp; Culture</a></p>
+        <p><a href='/spanish'>Latin American and Iberian Language, Literature, &amp; Culture</a></p>
+        <p><a href='/mathematics'>Mathematics</a></p>
+        <p><a href='/medieval'>Medieval Studies</a></p>
+        <p><a href='/music'>Music</a></p>
+        <p><a href='/kellogg-kroc'>Peace Studies</a></p>
+        <p><a href='/philosophy'>Philosophy</a></p>
+        <p><a href='/chemistry'>Physics</a></p>
+        <p><a href='/political-science'>Political Science</a></p>
+        <p><a href='/psychology'>Psychology</a></p>
+        <p><a href='/russian'>Russian and East European Language, Literature, &amp; Culture</a></p>
+        <p><a href='/sociology'>Sociology</a></p>
+        <p><a href='/theology-religion'>Theology and Religion</a></p>
+      </div>
+    </div>
+  )
+}
+
+export default SubjectList
