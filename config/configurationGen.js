7a8ea8e54a0c62aa26ab6bc1781b343c12aa667e
Jon Hartzler <jon.hartzler@gmail.comcoke>
2017-06-21 14:47:57 -0400

    fixed the slash and brought the not found back.

--- config/configurationGen.js
+++ config/configurationGen.js
@@ -6,7 +6,7 @@ const genConfig = () => {
     personAPI: 'https://suwnxx7rbi.execute-api.us-east-1.amazonaws.com/dev',
     recommendAPI: 'https://otlwuy48s9.execute-api.us-east-1.amazonaws.com/dev',
     coursesAPI: 'https://9er0bj2th7.execute-api.us-east-1.amazonaws.com/dev/',
-    resourcesAPI: 'https://p5gotxkf09.execute-api.us-east-1.amazonaws.com/dev',
+    resourcesAPI: 'https://p5gotxkf09.execute-api.us-east-1.amazonaws.com/dev/',
     illiadBaseURL: 'https://nd.illiad.oclc.org/illiad/IND/testweb/illiad.dll?Action=10&Form=<<form>>&Value=<<value>>',
     hoursAPIURL: 'https://g72i6y2qnj.execute-api.us-east-1.amazonaws.com/dev',
     contentfulAPI: 'https://bj5rh8poa7.execute-api.us-east-1.amazonaws.com/dev',
--- src/components/Messages/NotFound/index.js
+++ src/components/Messages/NotFound/index.js
@@ -6,7 +6,7 @@ import Link from '../../Link'
 
 const NotFound = ({ message = 'The requested page could not be found' }) => (
   <div className={'NotFound content'}>
-    <SearchProgramaticSet open={false} />
+    <SearchProgramaticSet open={true} />
     <PageTitle title='Not Found' />
     <div className='notfound'>
       <h1>404</h1>
