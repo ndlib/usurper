f33198ebf078e6234ea262fb535e10fe5e200879
Harrison Beachey <hbeachey@nd.edu>
2017-06-26 13:19:45 -0400

    cleanup page links and map links

--- src/components/Navigation/data/about.js
+++ src/components/Navigation/data/about.js
@@ -5,34 +5,28 @@ export const aboutData = [
     title: 'People',
     classes: 'col-md-offset-2 col-md-2',
     items: [
-    { title: 'Contact Us', url: '/get-help' },
-    { title: 'Faculty & Staff Directory', url: 'http://library.nd.edu/directory' },
-    { title: 'Employment Opportunities', url: '/employment' },
-    
-    
-    
+      { title: 'Contact Us', url: '/get-help' },
+      { title: 'Faculty & Staff Directory', url: 'http://library.nd.edu/directory' },
+      { title: 'Employment Opportunities', url: '/employment' },
     ],
   },
   {
     title: 'Spaces',
     classes: 'col-md-3',
     items: [
-    { title: 'Floor Maps & Locations', url: '/maps' },
-    { title: 'Hesburgh Library Renovation', url: 'http://renovation.library.nd.edu' },
-
+      { title: 'Floor Maps & Locations', url: '/maps' },
+      { title: 'Hesburgh Library Renovation', url: 'http://renovation.library.nd.edu' },
     ],
   },
   {
     title: 'Library Leadership',
     classes: 'col-md-3',
     items: [
-    { title: 'Mission and Vision', url: '/' },
-    { title: 'Library Giving', url: 'http://librarygiving.nd.edu' },
-    { title: 'University Committee on Libraries', url: '/ucl' }, 
-    
-
+      { title: 'Mission and Vision', url: '/mission-vision' },
+      { title: 'Library Giving', url: 'http://librarygiving.nd.edu' },
+      { title: 'University Committee on Libraries', url: '/ucl' },
     ],
   },
-  
-  
+
+
 ]
