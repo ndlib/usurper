27458b25abb67a67164bfecd65666b81e648114d
Justin Gondron <jgondron@nd.edu>
2017-06-27 16:45:42 -0400

    Quick fix of a bug with hours not displaying saturday label. Dev hours api was pointing to an instance that is no longer maintained. Changed to point to the correct dev api. Also updated corpfs

--- config/configurationGen.js
+++ config/configurationGen.js
@@ -8,7 +8,7 @@ const genConfig = () => {
     coursesAPI: 'https://unauxwjiml.execute-api.us-east-1.amazonaws.com/dev',
     resourcesAPI: 'https://p5gotxkf09.execute-api.us-east-1.amazonaws.com/dev',
     illiadBaseURL: 'https://nd.illiad.oclc.org/illiad/IND/testweb/illiad.dll?Action=10&Form=<<form>>&Value=<<value>>',
-    hoursAPIURL: 'https://g72i6y2qnj.execute-api.us-east-1.amazonaws.com/dev',
+    hoursAPIURL: 'https://3nmbw29969.execute-api.us-east-1.amazonaws.com/dev',
     contentfulAPI: 'https://bj5rh8poa7.execute-api.us-east-1.amazonaws.com/dev',
   }
 
