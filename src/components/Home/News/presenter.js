42517cde77ee871815377f83c56516ff55589cd1
Jon Hartzler <jon.hartzler@gmail.comcoke>
2017-07-06 15:38:17 -0400

    update news and events to read better.

--- src/components/Home/News/presenter.js
+++ src/components/Home/News/presenter.js
@@ -6,26 +6,28 @@ import './style.css'
 
 const News = (entries) => {
   return (
-    <section className='col-md-8 col-xs-12' aria-label="News">
+    <div className='col-md-8 col-xs-12'>
       <h3>News</h3>
-      {
-        entries.map((entry) => {
-          return (
-            <Link key={entry.fields.slug} title={entry.fields.title} to={'/news/' + entry.fields.slug}>
-              <div className='news-card'>
-                <Image cfImage={entry.fields.image} />
-                <header>
-                  <h3>{entry.fields.title}</h3>
-                </header>
-                <div className='description'>
-                  <LibMarkdown>{entry.fields.shortDescription}</LibMarkdown>
+      <section aria-label='News'>
+        {
+          entries.map((entry) => {
+            return (
+              <Link key={entry.fields.slug} ariaLabel={entry.fields.title} to={'/news/' + entry.fields.slug}>
+                <div className='news-card'>
+                  <Image cfImage={entry.fields.image} />
+                  <header>
+                    <h3>{entry.fields.title}</h3>
+                  </header>
+                  <div className='description'>
+                    <LibMarkdown>{entry.fields.shortDescription}</LibMarkdown>
+                  </div>
                 </div>
-              </div>
-            </Link>
-          )
-        })
-      }
-    </section>
+              </Link>
+            )
+          })
+        }
+      </section>
+    </div>
   )
 }
 
