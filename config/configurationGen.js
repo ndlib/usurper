bec4576fb15db17dda8a2ccffcfc0caa7f16c827
Harrison Beachey <hbeachey@nd.edu>
2017-07-11 11:24:56 -0400

    remove person api from the code

--- config/configurationGen.js
+++ config/configurationGen.js
@@ -3,7 +3,6 @@
 const genConfig = () => {
   let defaultConfig = {
     viceroyAPI: 'https://viceroy-dev.library.nd.edu',
-    personAPI: 'https://suwnxx7rbi.execute-api.us-east-1.amazonaws.com/dev',
     recommendAPI: 'https://otlwuy48s9.execute-api.us-east-1.amazonaws.com/dev',
     coursesAPI: 'https://unauxwjiml.execute-api.us-east-1.amazonaws.com/dev',
     resourcesAPI: 'https://p5gotxkf09.execute-api.us-east-1.amazonaws.com/dev',
@@ -14,7 +13,6 @@ const genConfig = () => {
 
   let config = {
     viceroyAPI: process.env.VICEROY_API ? process.env.VICEROY_API : defaultConfig.viceroyAPI,
-    personAPI: process.env.PERSON_API ? process.env.PERSON_API : defaultConfig.personAPI,
     recommendAPI: process.env.RECOMMEND_API ? process.env.RECOMMEND_API : defaultConfig.recommendAPI,
     coursesAPI: process.env.COURSES_API ? process.env.COURSES_API : defaultConfig.coursesAPI,
     resourcesAPI: process.env.RESOURCES_API ? process.env.RESOURCES_API : defaultConfig.resourcesAPI,
