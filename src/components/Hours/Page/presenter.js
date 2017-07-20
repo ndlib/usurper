1e751f97dddc3bf10877a6c1538457a866cb6418
Jon Hartzler <jon.hartzler@gmail.comcoke>
2017-07-20 16:11:11 -0400

    Move the code from the presenter to the container.

--- src/components/Hours/Page/presenter.js
+++ src/components/Hours/Page/presenter.js
@@ -8,26 +8,6 @@ import PageTitle from '../../PageTitle'
 import StaticSidebar from '../../Contentful/StaticContent/Sidebar'
 import StaticBody from '../../Contentful/StaticContent/Body'
 
-const hoursPageOrder = [
-  { servicePointSlug: 'hesburghlibrary', main: true },
-  { servicePointSlug: 'askusdesk', main: false },
-  { servicePointSlug: 'circulationservicedesk', main: false },
-  { servicePointSlug: 'oitoutpost', main: false },
-  { servicePointSlug: 'reservesmicrotextandmediadesk', main: false },
-  { servicePointSlug: 'architecturelibrary', main: true },
-  { servicePointSlug: 'byzantinestudiesreadingroom', main: true },
-  { servicePointSlug: 'centerfordigitalscholarship', main: true },
-  { servicePointSlug: 'centerfordigitalscholarshiprooms', main: false },
-  { servicePointSlug: 'chemistryphysicslibrary', main: true },
-  { servicePointSlug: 'engineeringlibrary', main: true },
-  { servicePointSlug: 'kelloggkroclibrary', main: true },
-  { servicePointSlug: 'mahaffeybusinesslibrary', main: true },
-  { servicePointSlug: 'medievalinstitutelibrary', main: true },
-  { servicePointSlug: 'musiclibrary', main: true },
-  { servicePointSlug: 'radiationchemistryreadingroom', main: true },
-  { servicePointSlug: 'visualresourcescenter', main: true },
-]
-
 const Presenter = (props) => {
   return (
     <div className='content hours-page'>
@@ -38,12 +18,12 @@ const Presenter = (props) => {
         <div className='col-md-8 col-sm-7'>
           <div className='service-point-list'>
             {
-              hoursPageOrder.map((servicePointOrder) => {
+              props.hoursPageOrder.map((servicePointOrder) => {
                 let sp = props.servicePoints[servicePointOrder.servicePointSlug]
                 if (!sp) {
                   return null
                 }
-                
+
                 return (<div key={sp.sys.id} className={servicePointOrder.main ? 'main-service-point' : 'sub-service-point'}>
                   <CurrentHours servicePoint={sp} >
                     <h4>{sp.fields.address}</h4>
