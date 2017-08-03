af1ce040f0a65fde38c1d7858a2fc1631c8a3806
Max Hallwachs <mhallwac@nd.edu>
2017-08-03 15:45:07 -0400

    finishes the scenarios for testing the Courses mapStateToProps method

--- src/tests/components/Courses/index.test.js
+++ src/tests/components/Courses/index.test.js
@@ -2,14 +2,14 @@ import * as statuses from '../../../constants/APIStatuses'
 import { mapStateToProps } from '../../../components/Courses'
 
 describe('components/Courses/index.js', () => {
-  describe('with data', () => {
+  describe('with correct data', () => {
     let state = {
       personal: {
         login: {
           state: statuses.SUCCESS,
         },
+        courses: ['courses']
       },
-      courses: ['courses']
     }
 
     let ownProp = {
@@ -29,7 +29,31 @@ describe('components/Courses/index.js', () => {
     })
 
     it('should have courses', () => {
-      expect(mapStateToProps(state, ownProp).courses).toEqual(['courses'])
+      expect(mapStateToProps(state, ownProp).courses).toEqual(state.personal.courses)
     })
-  }
-}
+  })
+
+  describe('with incorrect data', () => {
+    let state = {
+      personal: {
+        login: {
+          state: statuses.NOT_FOUND,
+        },
+      },
+    }
+
+    let ownProp = {}
+
+    it('should identify if the user is logged in', () => {
+      expect(mapStateToProps(state, ownProp).loggedIn).toBe(false)
+    })
+
+    it('should not be previewable', () => {
+      expect(mapStateToProps(state, ownProp).preview).toBe(false)
+    })
+
+    it('should not have courses', () => {
+      expect(mapStateToProps(state, ownProp).courses).toEqual({state: statuses.NOT_FETCHED})
+    })
+  })
+})
