0e9fe598b99ffb4b14cf367dc1410aeb806e5b0e
Max Hallwachs <mhallwac@nd.edu>
2017-08-14 12:48:58 -0400

    finished the test for Courses Container in index.js

--- src/tests/components/Courses/index.test.js
+++ src/tests/components/Courses/index.test.js
@@ -1,7 +1,120 @@
+import React from 'react'
 import * as statuses from '../../../constants/APIStatuses'
 import { mapStateToProps } from '../../../components/Courses'
+import CoursesPresenter from '../../../components/Courses/presenter'
+import { mount, shallow } from 'enzyme'
+import { CoursesContainer } from '../../../components/Courses'
+import Link from '../../../components/Link'
+import Loading from '../../../components/Messages/Loading'
 
-describe('components/Courses/index.js', () => {
+const setup = (props) => {
+  return shallow(<CoursesContainer {...props} />, { lifecycleExperimental: true })
+}
+
+let enzymeWrapper
+let props
+describe('components/Courses/index.js -- CoursesContainer', () => {
+  describe('Courses Container - not link only', () => {
+    beforeEach(() => {
+      props = {
+        linkOnly: false,
+        preview: true,
+        loggedIn: false,
+        login: {
+          redirectUrl: '',
+          token: 'fake token',
+          state: statuses.SUCCESS,
+        },
+        courses: {
+          state: statuses.SUCCESS,
+        },
+      }
+      enzymeWrapper = setup(props)
+    })
+
+    afterEach(() => {
+      enzymeWrapper = undefined
+    })
+
+    it('renders Courses Presenter', () => {
+      expect(enzymeWrapper
+        .containsMatchingElement(<CoursesPresenter {...props} />))
+        .toBe(true)
+    })
+  })
+
+  describe('Courses Container - not link only', () => {
+    beforeEach(() => {
+      props = {
+        linkOnly: true,
+        preview: true,
+        loggedIn: false,
+        login: {
+          redirectUrl: '',
+          token: 'fake token',
+          state: statuses.SUCCESS,
+        },
+        courses: {
+          state: statuses.SUCCESS,
+        },
+      }
+      enzymeWrapper = setup(props)
+    })
+
+    afterEach(() => {
+      enzymeWrapper = undefined
+    })
+
+    it('does not render Courses Presenter', () => {
+      expect(enzymeWrapper
+        .containsMatchingElement(<CoursesPresenter {...props} />))
+        .toBe(false)
+    })
+
+    it('only renders a link', () => {
+      expect(enzymeWrapper
+        .containsMatchingElement(<Link to='/courses' className='button fright tab'>My Courses</Link>))
+        .toBe(true)
+    })
+  })
+
+  describe('Courses Container - Loading', () => {
+    beforeEach(() => {
+      props = {
+        linkOnly: false,
+        preview: true,
+        loggedIn: false,
+        login: {
+          redirectUrl: '',
+          token: 'fake token',
+          state: statuses.SUCCESS,
+        },
+        courses: {
+          state: statuses.NOT_FETCHED,
+        },
+      }
+      enzymeWrapper = setup(props)
+    })
+
+    afterEach(() => {
+      enzymeWrapper = undefined
+    })
+
+    it('does not render Courses Presenter', () => {
+      expect(enzymeWrapper
+        .containsMatchingElement(<CoursesPresenter {...props} />))
+        .toBe(false)
+    })
+
+    it('renders Loading html', () => {
+      expect(enzymeWrapper
+        .containsMatchingElement(<Loading />))
+        .toBe(true)
+    })
+  })
+})
+
+describe('components/Courses/index.js -- mapStateToProps', () => {
   describe('with correct data', () => {
     let state = {
       personal: {
