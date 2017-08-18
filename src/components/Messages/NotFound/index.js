47f7607459e3dff64e5cfc56d862bcc23c4538a8
Dan Wolfe <dwolfe2@nd.edu>
2017-08-18 13:53:40 -0400

    Change link on 404 page
    * change to point to Library Website Content Review
    * add the URL that resulted in the 404 page to the ticket

--- src/components/Messages/NotFound/index.js
+++ src/components/Messages/NotFound/index.js
@@ -7,13 +7,13 @@ import ServiceNowLink from '../../ServiceNowLink'
 
 const NotFound = ({ message = 'The requested page could not be found' }) => (
   <div className={'NotFound content'}>
-    <SearchProgramaticSet open={true} />
+    <SearchProgramaticSet open />
     <PageTitle title='Page Not Found' />
     <div className='notfound'>
       <h1>404</h1>
       <h2>Page Not Found</h2>
       <div>{ message }<br />
-        If you think you've reached this page in error, please <ServiceNowLink>report your problem</ServiceNowLink>.
+        If you think you've reached this page in error, please <ServiceNowLink isWebContent>report your problem</ServiceNowLink>.
       </div>
     </div>
   </div>
