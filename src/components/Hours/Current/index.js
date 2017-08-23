be30bd69c1db545af1db16c645fb7e44194b4d07
Justin Gondron <jgondron@nd.edu>
2017-08-23 10:45:04 -0400

    - Added a way to adjust the date when the location closes at midnight so that the time comparisons work correctly.

--- src/components/Hours/Current/index.js
+++ src/components/Hours/Current/index.js
@@ -9,8 +9,9 @@ import makeGetHoursForServicePoint from '../../../selectors/hours'
 import * as statuses from '../../../constants/APIStatuses'
 import InlineContainer from '../InlineContainer'
 
-// Parses date/time from the strings given in an hoursEntry.
-const timeToday = (dateString, timeString, utcOffset) => {
+// Parses date/time from the strings given in an hoursEntry. midnightDayOffset specifies an adjustment
+// to the day when the time given is "00:MM". This should generally only be set to +1 for close dates
+const timeToday = (dateString, timeString, utcOffset, midnightDayOffset) => {
   // This does not account for time zone differences
   // We have to split the strings instead of concating as [date]T[time] because different browsers
   //   parse the timezone differently if it's not defined (UTC vs local)
@@ -20,9 +21,14 @@ const timeToday = (dateString, timeString, utcOffset) => {
   const offsetArray = utcOffset.split(':')
   const offsetHrs = parseInt(offsetArray[0])
   const offsetMins = (offsetHrs * 60) + parseInt(offsetArray[1])
-  // Month is month - 1 because date month is 0 based
-  let time = new Date(Number(dateArray[0]), Number(dateArray[1]) - 1, Number(dateArray[2]),
-                          Number(timeArray[0]), Number(timeArray[1]))
+
+  const hour = Number(timeArray[0])
+  const minute = Number(timeArray[1])
+  const year = Number(dateArray[0])
+  const month = Number(dateArray[1]) - 1 // Month is month - 1 because date month is 0 based
+  const day = Number(dateArray[2]) + (hour === 0 ? midnightDayOffset : 0)
+  let time = new Date(year, month, day, hour, minute)
+
   // The difference in offsets between local time and the api's time, in minutes
   let offsetDeltaMins = -offsetMins - time.getTimezoneOffset()
   return new Date(time.getTime() + (offsetDeltaMins * 60000))
@@ -82,8 +88,8 @@ export class CurrentHoursContainer extends Component {
     try {
       const entry = props.hoursEntry
       const currentOpenBlocks = entry.today.hours.filter(hoursBlock => {
-        let opens = timeToday(hoursBlock.date, hoursBlock.opens, props.hoursEntry.utcOffset)
-        let closes = timeToday(hoursBlock.date, hoursBlock.closes, props.hoursEntry.utcOffset)
+        let opens = timeToday(hoursBlock.date, hoursBlock.opens, props.hoursEntry.utcOffset, 0)
+        let closes = timeToday(hoursBlock.date, hoursBlock.closes, props.hoursEntry.utcOffset, 1)
         let now = new Date()
         if (opens <= now && now <= closes) {
           return true
