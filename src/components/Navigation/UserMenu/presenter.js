2296826f804c835585ee7e74e94a36a985f4e21c
Harrison Beachey <hbeachey@nd.edu>
2017-08-29 13:07:02 -0400

    remove link to settings menu

--- src/components/Navigation/UserMenu/presenter.js
+++ src/components/Navigation/UserMenu/presenter.js
@@ -11,7 +11,6 @@ const UserMenu = (props) => {
           <ul className='child'>
             <li><Link to='/items-requests'>Items &amp; Requests</Link></li>
             <li><Link to='/courses'>Courses</Link></li>
-            <li><Link to='/settings'>Settings</Link></li>
             <li>
               <Link
                 to={props.logoutUrl}
