10e7a4e77f12bf9fa2ea02259ff3c89d2d000eee
Dan Wolfe <dwolfe2@nd.edu>
2017-10-26 09:39:59 -0400

    LW-148 Fix heading issue in theme
    * Use div instead of h2 and h3 in the branding bar.
    * Adjust styles to conform to change

--- src/components/Header/BrandingBanner/index.js
+++ src/components/Header/BrandingBanner/index.js
@@ -8,8 +8,8 @@ class HomeBanner extends Component {
       <div id='wrapper'>
         <header className='gtype blue container-fluid' role='navigation' title='University of Notre Dame' id='header'>
           <hgroup>
-            <h3 className='nddotedu'><a href='http://nd.edu'>University <i>of</i> Notre Dame</a></h3>
-            <h2 className='dept-prov'><a href='http://provost.nd.edu'>Office <i>of the</i> Provost</a></h2>
+            <div className='nddotedu'><a href='http://nd.edu'>University <i>of</i> Notre Dame</a></div>
+            <div className='dept-prov'><a href='http://provost.nd.edu'>Office <i>of the</i> Provost</a></div>
           </hgroup>
         </header>
       </div>
