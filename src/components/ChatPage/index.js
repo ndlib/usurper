4a03870a9b3aa183a9f71acc09d422c81e8bbf45
Jon Hartzler <jon.hartzler@gmail.comcoke>
2017-10-27 10:17:49 -0400

    Fix text

--- src/components/ChatPage/index.js
+++ src/components/ChatPage/index.js
@@ -6,7 +6,7 @@ import SearchProgramaticSet from '../SearchProgramaticSet'
 const ChatPage = () => {
   return (
     <div className='chat-page'>
-      <PageTitle title='Chat with a Librarian' />
+      <PageTitle title='Chat with us' />
       <SearchProgramaticSet open={false} />
       <p>Chat is normally staffed 9:00 a.m. to 10:00 p.m.</p>
       <Chat />
