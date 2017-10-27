fb6a6e1c7f1c2af6df1f3698461451b15f4e8fb7
Jon Hartzler <jon.hartzler@gmail.comcoke>
2017-10-27 11:28:14 -0400

    Update how the chat css is done

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -6,7 +6,7 @@ const Chat = () => {
       <div className='libraryh3lp'>
         <iframe
           title="Chat with Us"
-          src='https://libraryh3lp.com/chat/nd-ask-a-lib@chat.libraryh3lp.com?skin=10273&amp;theme=gota&amp;title=Chat%20with%20us&amp;identity=asklib&amp;css=https://www.library.nd.edu/local_css/chat.css'
+          src='https://libraryh3lp.com/chat/nd-ask-a-lib@chat.libraryh3lp.com?skin=10273&amp;theme=gota&amp;title=Chat%20with%20us&amp;identity=asklib&amp;css=https://resources.library.nd.edu/css/chat.css'
           frameBorder={0}
           style={{
             width: '100%',
