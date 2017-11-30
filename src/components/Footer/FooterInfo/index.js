9aa2c9260b9f12552de0a5f83107652bbd7c8a75
Nathan-Ladwig <nladwig@nd.edu>
2017-11-30 10:47:22 -0500

    Changed Hour COlor and Deleted Test Phone Number

--- src/components/Footer/FooterInfo/index.js
+++ src/components/Footer/FooterInfo/index.js
@@ -14,7 +14,6 @@ const FooterInfo = () => {
               <p role='copyright'><Link to='https://www.nd.edu/copyright/'>Copyright</Link> © 2017 University of Notre Dame</p>
               <p><Link to='https://www.google.com/maps/place/Theodore+M.+Hesburgh+Library/@41.7023619,-86.2363832,17z/data=!3m1!4b1!4m5!3m4!1s0x8816d29f1af60a29:0x87f74f541c574744!8m2!3d41.7023579!4d-86.2341945'>221 Hesburgh Library, Notre Dame, IN 46556 </Link></p>
               <p>Phone <a href='tel:5746316679'>(574) 631-6679</a></p>
-              <p>Phone <a href='tel:5746316679'>(574) 631-6678</a></p>
             </div>
           </div>
           <div className='col-xs-12 col-md-4'>
