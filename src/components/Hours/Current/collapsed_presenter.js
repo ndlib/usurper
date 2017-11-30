14a854dae761198cca2e8b13568e0e71244bb635
Nathan-Ladwig <nladwig@nd.edu>
2017-11-30 11:11:28 -0500

    Changed ID to className

--- src/components/Hours/Current/collapsed_presenter.js
+++ src/components/Hours/Current/collapsed_presenter.js
@@ -33,7 +33,7 @@ const Presenter = (hoursEntry, isOpen, expandHandler, children) => {
           </div>
         </h4>
       </a>
-      <div id={hoursEntry.servicePoint.slug} role='tabpanel' aria-hidden={true} />
+      <div className={hoursEntry.servicePoint.slug} role='tabpanel' aria-hidden={true} />
     </section>
   )
 }
