c73202c92a71aafd2aaccd6b944ede5717849ffb
Ryan Doughty <rdought1@nd.edu>
2018-01-12 11:56:57 -0500

    contentful person and event migration

---
+++ deploy/contentfulMigrations/2018.01.11.addPerson.js
@@ -0,0 +1,59 @@
+const forward = (migration) => {
+
+  // PERSON type
+  const person = migration.createContentType('person')
+    .name('Person')
+    .displayField('name')
+
+  person.createField('name')
+    .type('Symbol')
+    .name('Name')
+
+  person.createField('title')
+    .type('Symbol')
+    .name('Title')
+
+  person.createField('bio')
+    .type('Text')
+    .name('Bio')
+
+  person.createField('email')
+    .type('Symbol')
+    .name('Email')
+
+  person.createField('phone')
+    .type('Symbol')
+    .name('Phone')
+
+  person.createField('picture')
+    .type('Link')
+    .name('Picture')
+    .linkType('Asset')
+
+  person.createField('type')
+    .type('Symbol')
+    .name('Type')
+    .validations([
+      { in: ['presenter'] },
+    ])
+
+  // EVENT field update
+  const eventType = migration.editContentType('event')
+    .createField('presenters')
+    .name('Presenter(s)')
+    .type('Array')
+    .items({
+      type: 'Link',
+      validations: [{
+        linkContentType: ['person'],
+      }],
+      linkType: 'Entry',
+    })
+}
+
+const reverse = (migration) => {
+  migration.deleteContentType('person')
+  migration.editContentType('event').deleteField('presenters')
+}
+
+module.exports = forward
