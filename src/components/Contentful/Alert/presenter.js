faa2b9d14d3071cf53c48c8660b574e3cddbe658
Harrison Beachey <hbeachey@nd.edu>
2018-01-22 16:24:08 -0500

    hopefully fix hound

--- src/components/Contentful/Alert/presenter.js
+++ src/components/Contentful/Alert/presenter.js
@@ -1,37 +1,42 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import Link from '../../Link'
 
 const Alert = ({ alerts }) => {
   return (
     <div className='alerts'>
-    {
-      // go through each alert type, putting all similar alerts into the same box
-      Object.keys(alerts).map(key => {
-        const currentAllerts = alerts[key]
-        const className = currentAllerts[0].className
+      {
+        // go through each alert type, putting all similar alerts into the same box
+        Object.keys(alerts).map(key => {
+          const currentAllerts = alerts[key]
+          const className = currentAllerts[0].className
 
-        return (
-          <div className={className} key={key}>
-            {
-              currentAllerts.map((alert, index) => {
-                return (
-                  <div className='width' key={key + '_' + alert.url + '_' + index}>
-                    <span className='prefix'>
-                      {currentAllerts[0].prefix}
-                    </span>
-                    <Link className='description' to={alert.url}>
-                      {alert.description}
-                    </Link>
-                  </div>
-                )
-              })
-            }
-          </div>
-        )
-      })
-    }
+          return (
+            <div className={className} key={key}>
+              {
+                currentAllerts.map((alert, index) => {
+                  return (
+                    <div className='width' key={key + '_' + alert.url + '_' + index}>
+                      <span className='prefix'>
+                        { currentAllerts[0].prefix }
+                      </span>
+                      <Link className='description' to={alert.url}>
+                        { alert.description }
+                      </Link>
+                    </div>
+                  )
+                })
+              }
+            </div>
+          )
+        })
+      }
     </div>
   )
+}
+
+Alert.propTypes = {
+  alerts: PropTypes.object,
 }
 
 export default Alert
