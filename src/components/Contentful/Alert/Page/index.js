b86649c78c62da86927a8d1d9039c1a0ae836f3a
Harrison Beachey <hbeachey@nd.edu>
2018-01-22 15:51:44 -0500

    add alert level, needs styling tweaks

--- src/components/Contentful/Alert/Page/index.js
+++ src/components/Contentful/Alert/Page/index.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import Presenter from '../presenter'
+import { alertMap, alertSort, alertCatagorize } from '../alertHelpers.js'
 
-const Alert = (props) => {
-  return (<Presenter {...props} />)
+const mapStateToProps = (state, ownProps) => {
+  let alerts = []
+
+  if (ownProps.alert) {
+    let alert = ownProps.alert.fields
+
+    let now = new Date()
+    let start = new Date(alert.startTime)
+    let end = new Date(alert.endTime)
+    if (start <= now && end >= now) {
+      alerts.push(alertMap(alert))
+    }
+
+    alerts.sort(alertSort)
+
+    alerts = alertCatagorize(alerts)
+  }
+  return {
+    alerts: alerts,
+  }
 }
 
-export default Alert
+class AlertContainer extends Component {
+  render () {
+    return (<Presenter {...this.props} />)
+  }
+}
+
+AlertContainer.propTypes = {
+  alerts: PropTypes.object,
+}
+
+export default connect(mapStateToProps)(AlertContainer)
---
+++ src/components/Contentful/Alert/alertHelpers.js
@@ -0,0 +1,62 @@
+import React from 'react'
+
+import alertImage from '../../../static/images/alert.png'
+import infoImage from '../../Footer/FooterLinks/images/ic_info_outline_white_24px.svg'
+
+const alertMap = (alert, isGlobal = false) => {
+  const warnPrefix = (<div><img src={alertImage} alt='Warning' aria-hidden /> </div>)
+  const infoPrefix = (<div><img src={infoImage} alt='Information' aria-hidden /> </div>)
+
+  let type = alert.type ? alert.type.toLowerCase() : 'warning'
+
+  let className = 'alert '
+  className += isGlobal ? 'global ' : 'page '
+  className += type
+
+  return {
+    ...alert,
+
+    className: className,
+    prefix: type === 'informational' ? infoPrefix : warnPrefix,
+
+    startTime: new Date(alert.startTime),
+    type: type,
+  }
+}
+
+const alertSort = (left, right) => {
+  if (left.type === right.type) {
+    if (left.startTime < right.startTime) {
+      return 1
+    } else if (right.startTime < left.startTime) {
+      return -1
+    }
+    return 0
+  }
+
+  // warnings should always be at the top
+  if (left.type === 'warning') {
+    return -1
+  }
+
+  return 1
+}
+
+const alertCatagorize = (alerts) => {
+  let out = {}
+  alerts.forEach((alert) => {
+    if (out[alert.type]) {
+      out[alert.type].push(alert)
+    } else {
+      out[alert.type] = [alert]
+    }
+  })
+
+  return out
+}
+
+module.exports = {
+  alertMap,
+  alertSort,
+  alertCatagorize,
+}
