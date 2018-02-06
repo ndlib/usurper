3833724d8ffe6fe5315a3f7ad60a0c20ebb14bca
Ryan Doughty <rdought1@nd.edu>
2018-02-06 16:15:58 -0500

    quick sponsorship cleanup

--- src/components/Sponsorships/index.js
+++ src/components/Sponsorships/index.js
@@ -2,15 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import LibMarkdown from '../LibMarkdown'
 
-const Sponsorships = ({ sponsors }) => {
-  console.log('SPONSORS: ' + JSON.stringify(sponsors))
-  return (
-    <div className='sponsors'>
-      <div><h4>Sponsored By</h4></div>
-      <div><LibMarkdown className='sponsor-detail'>{ sponsors }</LibMarkdown></div>
-    </div>
-  )
-}
+const Sponsorships = ({ sponsors }) => (
+  <div className='sponsors'>
+    <div><h4>Sponsored By</h4></div>
+    <div><LibMarkdown className='sponsor-detail'>{ sponsors }</LibMarkdown></div>
+  </div>
+)
 
 Sponsorships.propTypes = {
   sponsors: PropTypes.string.isRequired,
