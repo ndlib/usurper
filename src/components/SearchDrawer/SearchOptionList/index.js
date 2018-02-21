9c7495e027ed5c5a2b3ab222f6b2856cd7e1923b
Dan Wolfe <dwolfe2@nd.edu>
2018-02-21 14:27:46 -0500

    remove variable

--- src/components/SearchDrawer/SearchOptionList/index.js
+++ src/components/SearchDrawer/SearchOptionList/index.js
@@ -4,17 +4,19 @@ import SearchOption from './SearchOption'
 import { searchOptions } from '../searchOptions.js'
 
 const SearchOptionList = (props) => {
-  const options = searchOptions.map(
-    (item, index) => {
-      return (
-        <SearchOption {...props} item={item} key={index} index={index} />
-      )
-    }
-  )
   const classes = props.search.searchBoxOpen ? 'uSearchOptionList' : 'uSearchOptionList hidden'
-  return <span
-    className={classes}
-  >{ options }</span>
+
+  return (
+    <span className={classes}>{
+      searchOptions.map(
+        (item, index) => {
+          return (
+            <SearchOption {...props} item={item} key={index} index={index} />
+          )
+        }
+      )
+    }</span>
+  )
 }
 
 SearchOptionList.propTypes = {
