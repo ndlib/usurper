b2f16a246329ca66907bb4c49a93bd44c9544351
Harrison Beachey <hbeachey@nd.edu>
2018-03-08 09:45:40 -0500

    update comments to further clarify code

--- src/shared/ContentfulLibs.js
+++ src/shared/ContentfulLibs.js
@@ -23,11 +23,13 @@ export const flattenLocale = (fields, locale) => {
 
 // create an unifrom object to make a single mapping function easy in presenters
 export const getLinkObject = (fields, sysId) => {
-  // determine if heading should be a link
+  // We need to determine if the main heading (entry title) should be a link
+  //  The resource type has links in the "urls" json field. If there is only one, make it the heading link
+  //  If we're not handling a resource type the heading should be linked with its appropriate url field (below)
   let shouldHaveMain = !fields.urls || fields.urls.length === 1
   let mainUrl = (shouldHaveMain && fields.urls) ? fields.urls[0].url : ''
 
-  // get correct field for main link
+  // If we're not handling a resoruce type, get the link to use for the heading
   if (!mainUrl && shouldHaveMain) {
     if (fields.url) {
       mainUrl = fields.url
@@ -38,10 +40,12 @@ export const getLinkObject = (fields, sysId) => {
     }
   }
 
-  // get description field
+  // get appropriate description field for this content type
   let desc = fields.shortDescription ? fields.shortDescription : fields.description
 
-  // make list of links, either map all resource "urls" or a single link from the above mainurl
+  // Make a list of all links for this object, for the resource type it's the entire "urls" field
+  //  otherwise, we've already got the only link for the object in the mainUrl var
+  //  we also want to enrich the data with a title if there is none, and a keyId for use in displays <li key={keyId}>
   let links
   if (fields.urls) {
     links = fields.urls.map((data, index) => {
