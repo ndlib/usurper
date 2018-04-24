ab2663b85dfe624283d031c4adc2322d645ca5a6
Harrison Beachey <hbeachey@nd.edu>
2018-04-24 13:55:13 -0400

    update ci deployment and add web deployment configs

---
+++ deploy/web/postDeploy.py
@@ -0,0 +1,22 @@
+from hesburgh import scriptutil, heslog
+
+def run(stage, outputs):
+  bucket = None
+  for k, stackInfo in outputs.iteritems():
+    out = stackInfo.get("outputs", {})
+    if "BucketName" in out:
+      bucket = out.get("BucketName")
+      break
+
+  if not bucket:
+    heslog.error("no bucket found to deploy to")
+    return {}
+
+  heslog.info("Building Source")
+  scriptutil.executeCommand("yarn build")
+
+  heslog.info("Deploying code to bucket %s" % bucket)
+
+  scriptutil.executeCommand("cd ../../build/public && aws s3 sync . s3://%s --delete --acl public-read" % bucket)
+
+  return {}
---
+++ deploy/web/preDeploy.py
@@ -0,0 +1,41 @@
+from hesburgh import scriptutil, heslog
+import json
+import os
+
+def getCert(domain):
+  output = scriptutil.executeCommand("aws acm list-certificates --certificate-statuses ISSUED --includes extendedKeyUsage=TLS_WEB_SERVER_AUTHENTICATION --query 'CertificateSummaryList[?DomainName==`%s`]|[0]'" % domain)
+  data = json.loads(output.get("output"))
+  if data:
+    return data.get("CertificateArn")
+  return None
+
+
+def generateEnv(stage):
+  fqdn = "usurper" if stage == "prod" else "usurper-" + stage
+  fqdn = "%s.library.nd.edu" % fqdn
+
+  certArn = getCert(fqdn)
+  if not certArn:
+    certArn = getCert('*.library.nd.edu')
+
+  return {
+    "CERT_ARN": certArn,
+    "FQDN": fqdn,
+  }
+
+
+def runTests():
+  os.environ["CI"] = "Local"
+  output = scriptutil.executeCommand("yarn test")
+  return output.get("code") == 0
+
+
+def run(stage):
+  heslog.info("Running yarn")
+  scriptutil.executeCommand("cd ../.. && yarn")
+
+  heslog.info("Running tests")
+  if not runTests():
+    return { "error": "Testing Failed" }
+
+  return { "env": generateEnv(stage) }
