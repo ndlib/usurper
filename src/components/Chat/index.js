93ad56c3dc1986283b1923a8fda7684c9332cdca
Jon Hartzler <jon.hartzler@gmail.comcoke>
2018-05-02 14:03:33 -0400

    hide the chat window and added a clickable phone number

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Chat = () => {
   return (
     <section className='chat' aria-label='Chat with Us'>
-      <div className='libraryh3lp'>
+      <div className='skiplink'>
+        Phone Number: <a href="tel:5746316679" aria-label="Call (574) 631-6679">(574) 631-6679</a>
+      </div>
+      <div className='libraryh3lp' aria-hidden='true'>
         <iframe
           title="Chat with Us"
           src='https://libraryh3lp.com/chat/nd-ask-a-lib@chat.libraryh3lp.com?skin=10273'
