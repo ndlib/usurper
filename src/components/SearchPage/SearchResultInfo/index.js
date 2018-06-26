f1bb9592749d0f3c1421371a24ea542f1cc9cbc6
Jon Hartzler <jon.hartzler@gmail.comcoke>
2018-06-26 08:15:04 -0400

    Hound

--- src/components/SearchPage/SearchResultInfo/index.js
+++ src/components/SearchPage/SearchResultInfo/index.js
@@ -5,7 +5,9 @@ import PropTypes from 'prop-types'
 const SearchResultInfo = (props) => {
   if (props.searchInformation && props.searchInformation.totalResults > 0) {
     return (
-      <p className='search-result-info'>About {props.searchInformation.totalResults} results ({props.searchInformation.formattedSearchTime}  seconds)</p>
+      <p className='search-result-info'>
+        About {props.searchInformation.totalResults} results ({props.searchInformation.formattedSearchTime}  seconds)
+      </p>
     )
   }
   return null
