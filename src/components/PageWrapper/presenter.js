b1f6f2d77eddcf223351707659cf6f15a8e8d1bd
Andy Wetherill <awetheri@nd.edu>
2018-08-01 16:15:06 -0400

    Fixing some accessibility errors.

--- src/components/PageWrapper/presenter.js
+++ src/components/PageWrapper/presenter.js
@@ -7,11 +7,12 @@ import '../../static/css/flexboxgrid.css'
 import '../../static/css/global.css'
 import '../../static/css/print.css'
 import Config from '../../shared/Configuration'
+import spacer from '../../static/images/spacer.gif'
 const PageWrapper = (props) => {
   return (
     <div onClick={props.clickOnPage} className={window.location.pathname === '/' ? 'home' : 'not-home'}>
       <meta id='nd-version' content={Config.version} />
-      <a className='skiplink' href='#maincontent'>Skip to content</a>
+      <a href='#maincontent' className="skip"><img src={spacer} className="skip" width="0" height="0" border="0" alt="skip to content" /></a>
 
       <Header {...props} />
       <div id='maincontent' className={'container-fluid content'}>
