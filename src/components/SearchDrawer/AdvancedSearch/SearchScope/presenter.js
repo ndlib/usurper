dc50cbd1b625c6de819a9a3b4fead2c233f5cc47
Ryan Doughty <rdought1@nd.edu>
2018-08-20 12:43:29 -0400

    search scope

--- src/components/SearchDrawer/AdvancedSearch/SearchScope/presenter.js
+++ src/components/SearchDrawer/AdvancedSearch/SearchScope/presenter.js
@@ -6,21 +6,22 @@ const SearchScope = (props) => {
       <div>
         <label htmlFor='exlidSearchIn'>Search Scope:</label>
         <select id='scopesListAdvanced' name='scp.scps' onChange={props.onChange}>
-          <option value='scope%3A%28hathi_pub%29,scope%3A%28ndulawrestricted%29,scope%3A%28dtlrestricted%29,scope%3A%28NDU%29,scope%3A%28NDLAW%29,scope%3A%28ndu_digitool%29'>All Notre Dame Campus Libraries</option>
-          <option value='scope%3A%28NDU%29,scope%3A%28BCI%29,scope%3A%28HCC%29,scope%3A%28SMC%29,scope%3A%28NDLAW%29,scope%3A%28&quot;MALC&quot;%29'>ND and Partner Libraries</option>
-          <option value='scope%3A%28dtlrestricted%29,scope%3A%28NDU%29,scope%3A%28ndu_digitool%29,scope%3A%28&quot;MALC&quot;%29'>Hesburgh Libraries &amp; Branches</option>
-          <option value='scope%3A%28RARE%29,scope%3A%28MRARE%29,scope%3A%28SPEC%29'>Special Collections</option>
-          <option value='scope%3A%28MEDIN%29'>Medieval Institute</option>
-          <option value='scope%3A%28REF%29'>Hesburgh Reference</option>
-          <option value='scope%3A%28NDLAW%29'>Kresge Law Library</option>
-          <option value='scope%3A%28BIC%29'>Business Library</option>
-          <option value='scope%3A%28ARCHT%29'>Architecture Library</option>
-          <option value='scope%3A%28CHEMP%29'>Chemistry Physics Library</option>
-          <option value='scope%3A%28ENGIN%29' >Engineering Library</option>
-          <option value='scope%3A%28KELLO%29'>Kellogg/Kroc Information Center</option>
-          <option value='scope%3A%28MATH%29'>Mathematics Library</option>
-          <option value='scope%3A%28CSLC%29'>Center for the Study of Languages &amp; Culture</option>
-          <option value='scope%3A%28RADLB%29'>Radiation Laboratory</option>
+          <option value='nd_campus'>All Notre Dame Campus Libraries</option>
+          <option value='NDU'>Hesburgh Libraries &amp; Branches</option>
+          <option value='architecture'>Architecture Library</option>
+          <option value='bic'>Business Library</option>
+          <option value='cslc'>Center for the Study of Language &amp; Cultures</option>
+          <option value='chemphys'>Chemistry Physics Library</option>
+          <option value='engineering'>Engineering Library</option>
+          <option value='ref'>Hesburgh Reference</option>
+          <option value='kellog'>Kellogg Kroc Library</option>
+          <option value='ndlaw'>Kresge Law Library</option>
+          <option value='math'>Mathematics Library</option>
+          <option value='medieval'>Medieval Institute</option>
+          <option value='music'>Music</option>
+          <option value='radlab'>Radiation Laboratory</option>
+          <option value='spec_coll'>Rare Books &amp; Special Collections</option>
+          <option value='archv'>University Archives</option>
         </select>
       </div>
     )
