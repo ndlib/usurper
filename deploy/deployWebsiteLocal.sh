0f6840670836f55dbcfdabad6869abc438a5ff96
Jon Hartzler <jon.hartzler@gmail.comcoke>
2018-10-03 16:10:40 -0400

    reset index file after deploy

--- deploy/deployWebsiteLocal.sh
+++ deploy/deployWebsiteLocal.sh
@@ -49,9 +49,10 @@ else
   git checkout $(cat VERSION)
 fi
 
-sed -i 's/ENVIRONMENT/'$stage'/g' ../public/index.html
+# set sentry values
+sed -i '' 's/ENVIRONMENT/'$stage'/g' ../public/index.html
 version=$(cat ../VERSION)
-sed -i 's/SHA/'$version'/g' ../public/index.html
+sed -i '' 's/SHA/'$version'/g' ../public/index.html
 
 echo "install npm modules"
 yarn
@@ -59,6 +60,8 @@ yarn
 echo "build production"
 yarn build --production
 
+# reset sentry changes
+git checkout ../public/index.html
 
 echo "Push to bucket, $BUCKET"
 aws s3 sync --delete build/public s3://$BUCKET
