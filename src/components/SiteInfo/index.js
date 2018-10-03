598e1ca1d9aeb457a2bed5df138542504c928e6d
Jon Hartzler <jon.hartzler@gmail.comcoke>
2018-10-03 10:25:29 -0400

    Remove the info things we don't want.

--- src/components/SiteInfo/index.js
+++ src/components/SiteInfo/index.js
@@ -1,6 +1,4 @@
 import React, { Component } from 'react'
-import findIP from './findIP'
-import lastModified from './lastModified'
 import ServiceNowLink from '../ServiceNowLink'
 
 const VERSION = require('../../../VERSION')
@@ -15,20 +13,6 @@ class SiteInfo extends Component {
     }
   }
 
-  componentWillMount () {
-    findIP.then(
-      ip => {
-        this.setState({ ipAddress: ip })
-      }
-    )
-
-    lastModified.then(
-      modified => {
-        this.setState({ lastModified: modified })
-      }
-    )
-  }
-
   render () {
     return (
       <div id='site-info'>
@@ -37,9 +21,7 @@ class SiteInfo extends Component {
         <dl>
           <dt>Website Version:</dt> <dd>{version}</dd>
           <dt>React Version:</dt> <dd>{React.version}</dd>
-          <dt>Cache Last Modified:</dt> <dd>{this.state.lastModified}</dd>
           <dt>Referral Path:</dt> <dd>{window.location.search.toString().replace('?URL=', '')}</dd>
-          <dt>IP Address:</dt> <dd>{this.state.ipAddress}</dd>
           <dt>User Agent:</dt> <dd>{navigator.userAgent}</dd>
           <dt>Screen Dimensions:</dt> <dd>{window.screen.width}x{window.screen.height}</dd>
           <dt>Window Dimensions:</dt> <dd>{window.innerWidth}x{window.innerHeight}</dd>
