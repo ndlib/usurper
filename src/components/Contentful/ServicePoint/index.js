4ddb4fee1bc0d571f851ebbf7596b20dd9422ba4
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2018-11-01 11:35:28 -0400

    Show service point on floor map page according based on the building it is located in.
    If the ServicePoint component receives a reference with an id but is missing the full details (fields), go and fetch the service point explicitly from contentful.

--- src/components/Contentful/ServicePoint/index.js
+++ src/components/Contentful/ServicePoint/index.js
@@ -1,44 +1,82 @@
 // Container component for a Page content type from Contentful
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import PropTypes from 'prop-types'
 import CurrentHours from '../../Hours/Current'
 import Contact from '../../Contact/ServicePoint'
 import { withErrorBoundary } from '../../ErrorBoundary'
+import { fetchServicePoints } from '../../../actions/contentful/servicePoints'
+import * as statuses from '../../../constants/APIStatuses'
 
-const ServicePoint = ({ cfServicePoint, showDetails }) => {
-  if (!cfServicePoint || !cfServicePoint.fields) {
-    return null
+export class ServicePointContainer extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      fetchStatus: statuses.NOT_FETCHED,
+      sp: props.cfServicePoint
+    }
   }
 
-  return (
-    <section
-      aria-label={'Service Point: ' + cfServicePoint.fields.title}
-      role='complementary'
-      className='sp-address'
-      itemScope
-      itemType='http://schema.org/Place'
-    >
-      <h2 itemProp='name'>{cfServicePoint.fields.title}</h2>
+  componentDidMount () {
+    const self = this
+    if (this.state.fetchStatus === statuses.NOT_FETCHED && this.state.sp && !this.state.sp.fields) {
+      this.props.fetchServicePoints(false, this.state.sp.sys.id)
+      .then(function(response) {
+        self.setState({ fetchStatus: response.status })
+        if (response.servicePoints && response.servicePoints.length) {
+          self.setState({ sp: response.servicePoints[0] })
+        }
+      })
+      .catch(function(error) {
+        console.log('Error fetching service points')
+        self.setState({ fetchStatus: statuses.ERROR })
+      })
+    }
+  }
+
+  render() {
+    if (!this.state.sp || !this.state.sp.fields) {
+      return null
+    }
+
+    return (
+      <section
+        aria-label={'Service Point: ' + this.state.sp.fields.title}
+        role='complementary'
+        className='sp-address'
+        itemScope
+        itemType='http://schema.org/Place'
+      >
+        <h2 itemProp='name'>{this.state.sp.fields.title}</h2>
 
 
-      { showDetails && (
-        <div>
-          <h4 itemProp='streetAddress'><address>{cfServicePoint.fields.address}</address></h4>
-          <CurrentHours servicePoint={cfServicePoint} />
-        </div>
-      )}
-      <Contact servicePoint={cfServicePoint} showDetails={showDetails} />
-    </section>
-  )
+        { this.props.showDetails && (
+          <div>
+            <h4 itemProp='streetAddress'><address>{this.state.sp.fields.address}</address></h4>
+            <CurrentHours servicePoint={this.state.sp} />
+          </div>
+        )}
+        <Contact servicePoint={this.state.sp} showDetails={this.props.showDetails} />
+      </section>
+    )
+  }
 }
 
-ServicePoint.propTypes = {
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ fetchServicePoints }, dispatch)
+}
+
+ServicePointContainer.propTypes = {
   cfServicePoint: PropTypes.object.isRequired,
   showDetails: PropTypes.bool,
+  fetchServicePoints: PropTypes.func.isRequired,
 }
 
-ServicePoint.defaultProps = {
+ServicePointContainer.defaultProps = {
   showDetails: true,
 }
+
+const ServicePoint = connect(null, mapDispatchToProps)(ServicePointContainer)
 
 export default withErrorBoundary(ServicePoint)
