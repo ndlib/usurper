f85b4833b69f640d971512df6cd593aaf1eaf8ff
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2018-12-04 13:02:57 -0500

    D'oh

--- src/components/Messages/AccountExpired/index.js
+++ src/components/Messages/AccountExpired/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 const AccountExpired = ({ message = 'Please contact campus Card Services for more information.' }) => (
   <div className='alert status failure' style={{ marginBottom: '1.5em' }}>
