55c2e9f1ce9804151b5ca327577fcb34821dd058
Andy Wetherill <awetheri@nd.edu>
2019-01-21 11:14:28 -0500

    # 50-character subject line # # 72-character wrapped longer description. This should answer: # # * Why was this change necessary? # * How does it address the problem? # * Are there any side effects? # # Include a link to the ticket, if any.

--- src/components/SearchDrawer/AdvancedSearch/AdvancedSearchField/index.js
+++ src/components/SearchDrawer/AdvancedSearch/AdvancedSearchField/index.js
@@ -6,7 +6,7 @@ import SearchInput from './SearchInput'
 
 const AdvancedSearchField = (props) => {
   return (
-    <fieldset id={`exlidAdvancedSearchFieldset${props.id}`} ><div className="fgrid">
+    <fieldset id={`exlidAdvancedSearchFieldset${props.id}`} ><div className='fgrid'>
       <SearchField id={`scope_${props.id}`} />
       <MatchType id={`precisionOperator_${props.id}`} />
       <SearchInput id={`freeText_${props.id}`} /></div>
