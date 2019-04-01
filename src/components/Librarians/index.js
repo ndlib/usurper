c785b519df579ba72b5322b540a3d01c6f2ba2db
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2019-04-01 12:37:45 -0400

    Refactor loading contact info.

--- src/components/Librarians/index.js
+++ src/components/Librarians/index.js
@@ -1,30 +1,44 @@
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
+import PropTypes from 'prop-types'
 import { fetchLibrarians } from '../../actions/librarians'
 import React, { Component } from 'react'
 import LibrarianPresenter from './presenter.js'
 import * as statuses from '../../constants/APIStatuses'
 import { withErrorBoundary } from '../ErrorBoundary'
 const mapStateToProps = (state, ownProps) => {
+  const { librarianInfo } = state
+
+  const info = (librarianInfo && librarianInfo.netids) ? librarianInfo : { status: statuses.NOT_FOUND }
+
   return {
     // only those librarians for this page will affect this page
-    librarianInfo: ownProps.netids && ownProps.netids.equals(state.librarianInfo.netids) ? state.librarianInfo : { status: statuses.NOT_FOUND },
+    librarianInfo: info,
     ...ownProps,
   }
 }
 
-const mapDispatchToProps = (dispatch, ownProps) => {
+const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({ fetchLibrarians }, dispatch)
 }
 
 export class LibrariansContainer extends Component {
   componentDidMount () {
-    this.props.fetchLibrarians(this.props.netids)
+    if (this.props.librarianInfo.status !== statuses.FETCHING ||
+    !this.props.netids.equals(this.props.librarianInfo.netids)) {
+      this.props.fetchLibrarians(this.props.netids)
+    }
   }
 
   render () {
     return <LibrarianPresenter {...this.props} />
   }
+}
+
+LibrariansContainer.propTypes = {
+  netids: PropTypes.array,
+  librarianInfo: PropTypes.object,
+  fetchLibrarians: PropTypes.func,
 }
 
 const Librarians = connect(
