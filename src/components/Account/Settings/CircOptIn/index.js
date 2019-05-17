8d863061982363090e48526caa43c5c2860e9214
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2019-05-17 11:56:18 -0400

    Refactor settings and add unit tests.

--- src/components/Account/Settings/CircOptIn/index.js
+++ src/components/Account/Settings/CircOptIn/index.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import ReactModal from 'react-modal'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { KIND, getCircStatus, setCircStatus } from 'actions/personal/settings'
@@ -8,17 +7,18 @@ import * as statuses from 'constants/APIStatuses'
 import InlineLoading from 'components/Messages/InlineLoading'
 import UpdateStatus from 'components/Messages/UpdateStatus'
 
-ReactModal.setAppElement('body')
+import PolicyInfo from './PolicyInfo'
+import CircHistoryModal from './CircHistoryModal'
 
-class CircOptIn extends Component {
+export class CircOptInContainer extends Component {
   constructor (props) {
     super(props)
     this.confirmToggleStatus = this.confirmToggleStatus.bind(this)
     this.dismiss = this.dismiss.bind(this)
+    this.statusText = this.statusText.bind(this)
     this.state = {
       updated: false,
       modalOpen: false,
-      error: '',
     }
   }
 
@@ -50,153 +50,43 @@ class CircOptIn extends Component {
     this.props.setCircStatus(newValue)
     this.setState({
       updated: true,
-      error: '',
     })
   }
 
+  statusText () {
+    switch (this.props.updateStatus) {
+      case statuses.SUCCESS:
+        return this.props.optedIn ? 'Successfully opted in.' : 'Successfully opted out.'
+      case statuses.ERROR:
+        return 'Failed to update preference. Please refresh and try again.'
+      default:
+        return null
+    }
+  }
+
   render () {
-    if (this.props.circStatus === statuses.NOT_FETCHED || this.props.circStatus === statuses.FETCHING) {
+    if ([statuses.NOT_FETCHED, statuses.FETCHING].includes(this.props.circStatus)) {
       return <InlineLoading />
     }
-    const statusText = () => {
-      switch (this.props.updateStatus) {
-        case statuses.SUCCESS:
-          return this.props.optedIn ? 'Successfully opted in.' : 'Successfully opted out.'
-        case statuses.ERROR:
-          return 'Failed to update preference. Please refresh and try again.'
-        default:
-          return null
-      }
-    }
 
+    const buttonText = this.props.optedIn ? 'Opt-out and delete history' : 'Opt-in to save history'
+    const checkboxLabel = this.props.optedIn ? 'You have opted to save your checkout history.' : 'Your checkout history is not being saved.'
     return (
       <React.Fragment>
         <section className='group opt-in'>
           <h3>Checkout History</h3>
           <div className='section-box'>
-            <input
-              id='circ-opt-in'
-              type='checkbox'
-              checked={this.props.optedIn}
-              disabled
-            />
-            <label htmlFor='circ-opt-in'>
-              {this.props.optedIn
-                ? 'You have opted to save your checkout history.'
-                : 'Your checkout history is not being saved.'
-              }
-            </label>
-            <button className='right' onClick={this.openModal}>
-              {this.props.optedIn
-                ? 'Opt-out and delete history'
-                : 'Opt-in to save history'
-              }
-            </button>
-            <UpdateStatus className='pad-edges-md' status={this.props.updateStatus} text={statusText()} />
+            <input id='circ-opt-in' type='checkbox' checked={this.props.optedIn} disabled />
+            <label htmlFor='circ-opt-in'>{checkboxLabel}</label>
+            <button className='right' onClick={this.openModal}>{buttonText}</button>
+            <UpdateStatus className='pad-edges-md' status={this.props.updateStatus} text={this.statusText()} />
           </div>
           <hr />
-          <ul className='circ-policy'>
-            <li>
-              The Hesburgh Libraries values your privacy and the confidentiality of your borrowing records and
-              checkout history.
-            </li>
-            <li>
-              No borrower information is released to third parties including outside agencies or individuals
-              without a court order.
-            </li>
-            <li>
-              Please be aware that the library retains each item in your checkout history for 30 days after
-              it is returned, in case there are any issues. The library will not be able to purge your individual
-              records during th​at​ rolling 30​ day​ period.
-            </li>
-            <li>Interlibrary loan records are not able to be purged at any point.</li>
-            <li>
-              You may choose to have your checkout history retained and ​remain​ available to you ​as long as you
-              are affiliated with the University​ by opting in on this page.​ After you opt in, you may choose to
-              delete individual items, or opt out and delete your entire history.
-            </li>
-            <li>
-              Please note that once you opt-in to save your checkout history, only currently checked-out items,
-              items returned in the last 30 days, and any items you borrow in the future will be included in your
-              saved checkout history. Items returned more than 30 days ago have already been purged.
-            </li>
-          </ul>
+          <PolicyInfo />
         </section>
-        <ReactModal
-          isOpen={this.state.modalOpen}
-          shouldCloseOnEsc
-          onRequestClose={this.dismiss}
-          contentLabel='Checkout History Confirmation'
-          className='modal round-corners'
-          overlayClassName='modal-overlay'
-          ariaHideApp
-          aria={{
-            labelledby: 'checkoutHistoryModalTitle',
-            describedby: 'checkoutHistoryModalDesc',
-          }}
-          shouldFocusAfterRender
-          shouldReturnFocusAfterClose
-        >
-          {this.props.optedIn ? (
-            <div className='modal-body'>
-              <h2 id='checkoutHistoryModalTitle'>Opt-out of Checkout History?</h2>
-              <div id='checkoutHistoryModalDesc'>
-                You are selecting that you no longer wish to have your checkout history saved.
-                <ul>
-                  <li>
-                    Items in your history cannot be deleted until 30 days after they are returned.
-                  </li>
-                  <li>
-                    Interlibrary Loan records cannot be deleted.
-                  </li>
-                  <li>
-                    Deleted items are lost permanently and cannot be restored.
-                  </li>
-                  <li>
-                    If you want to save a copy of your checkout history before deleting it, please select cancel,
-                    and use the export button in your checkout history.
-                  </li>
-                </ul>
-                Do you still wish to proceed with opting out?
-              </div>
-            </div>
-          ) : (
-            <div className='modal-body'>
-              <h2 id='checkoutHistoryModalTitle'>Opt-in to Save Checkout History?</h2>
-              <div id='checkoutHistoryModalDesc'>
-                You are selecting that you no longer wish to have your checkout history saved.
-                <ul>
-                  <li>
-                    By opting in, you agree that the Hesburgh Libraries can retain a complete record of your
-                    checkout history. This history will only be removed at your request.
-                  </li>
-                  <li>
-                    Only currently checked-out items, items returned in the last 30 days,
-                    and any items you borrow in the future will be included in your saved checkout history.
-                    Items returned more than 30 days ago have already been purged.
-                  </li>
-                  <li>
-                    You may opt-out and delete your checkout history at any time.
-                  </li>
-                </ul>
-                Do you wish to proceed with opting in?
-              </div>
-            </div>
-          )}
-          <div className='modal-footer'>
-            { this.props.updating && (
-              <InlineLoading title='' />
-            )}
-            <button onClick={this.dismiss} disabled={this.props.updating}>Cancel</button>
-            <button
-              className={'danger' + (this.props.optedIn ? ' checkout-history-opt-out' : ' checkout-history-opt-in')}
-              onClick={this.confirmToggleStatus}
-              disabled={this.props.updating}
-            >
-              Confirm
-            </button>
-          </div>
-        </ReactModal>
+        <CircHistoryModal
+          isOpen={this.state.modalOpen} optedIn={this.props.optedIn} updating={this.props.updating}
+          onClose={this.dismiss} onConfirm={this.confirmToggleStatus} />
       </React.Fragment>
     )
   }
@@ -223,7 +113,7 @@ export const mapDispatchToProps = (dispatch) => {
   }, dispatch)
 }
 
-CircOptIn.propTypes = {
+CircOptInContainer.propTypes = {
   setCircStatus: PropTypes.func.isRequired,
   getCircStatus: PropTypes.func.isRequired,
   circStatus: PropTypes.string.isRequired,
@@ -231,4 +121,4 @@ CircOptIn.propTypes = {
   updateStatus: PropTypes.string.isRequired,
   updating: PropTypes.bool,
 }
-export default connect(mapStateToProps, mapDispatchToProps)(CircOptIn)
+export default connect(mapStateToProps, mapDispatchToProps)(CircOptInContainer)
---
+++ src/tests/components/Layout/PageWrapper/index.test.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import configureMockStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+
+import PageWrapper from 'components/Layout/PageWrapper'
+import Presenter from 'components/Layout/PageWrapper/presenter'
+
+import { CLOSE_SEARCHBOX } from 'actions/search'
+import { CLOSE_MENUS } from 'actions/menu'
+
+let enzymeWrapper
+let props
+let store
+
+const middlewares = [ thunk ]
+const mockStore = configureMockStore(middlewares)
+
+const setup = (state, ownProps) => {
+  store = mockStore(state)
+  return shallow(<PageWrapper store={store} {...ownProps} />)
+}
+
+describe('components/Layout/PageWrapper', () => {
+  beforeEach(() => {
+    const state = {
+      search: {
+        searchBoxOpen: true,
+      },
+      menus: {
+        openMenuId: 1,
+      },
+    }
+    enzymeWrapper = setup(state, null)
+  })
+
+  afterEach(() => {
+    enzymeWrapper = undefined
+  })
+
+  it('should render a Presenter', () => {
+    expect(enzymeWrapper.dive().dive().find(Presenter).exists()).toBe(true)
+  })
+
+  it('should close search and menus when clicking on page', () => {
+    enzymeWrapper.dive().props().clickOnPage()
+    expect(store.getActions()).toEqual(expect.arrayContaining([
+      { type: CLOSE_SEARCHBOX },
+      { type: CLOSE_MENUS },
+    ]))
+  })
+})
