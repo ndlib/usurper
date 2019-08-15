ead8156c8bad7b07ef1858ef9ddb53a50eecca00
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2019-08-15 15:57:05 -0400

    Proof of concept. (Not fully functional)

--- src/components/Contentful/Alert/Global/index.js
+++ src/components/Contentful/Alert/Global/index.js
@@ -2,51 +2,70 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import PropTypes from 'prop-types'
+import typy from 'typy'
 import { fetchAllAlerts } from 'actions/contentful/allAlerts'
+import { getHiddenAlerts } from 'actions/personal/settings'
 import { alertMap, alertSort, alertCatagorize } from '../alertHelpers.js'
 import Presenter from '../presenter.js'
 import * as statuses from 'constants/APIStatuses'
 import makeAlertSelector from 'selectors/alerts'
 import { withErrorBoundary } from 'components/ErrorBoundary'
 
-const makeMapStateToProps = () => {
-  const selector = makeAlertSelector()
+const mapStateToProps = (state) => {
+  const { personal, settings } = state
+  const alertSelector = makeAlertSelector()
+  const selected = alertSelector(state)
 
-  const mapStateToProps = (state) => {
-    const selected = selector(state)
+  let alerts = []
+  selected.forEach((entry) => {
+    alerts.push(alertMap({
+      id: entry.sys.id,
+      ...entry.fields,
+    }, true))
+  })
 
-    let alerts = []
-    selected.forEach((entry) => {
-      const alert = entry.fields
-      alerts.push(alertMap(alert, true))
-    })
+  alerts.sort(alertSort)
 
-    alerts.sort(alertSort)
+  alerts = alertCatagorize(alerts)
 
-    alerts = alertCatagorize(alerts)
-
-    return {
-      allAlerts: alerts,
-      allAlertsStatus: state.allAlerts ? state.allAlerts.status : '',
-    }
+  return {
+    allAlerts: alerts,
+    allAlertsStatus: state.allAlerts ? state.allAlerts.status : '',
+    hiddenAlerts: settings.hiddenAlerts,
+    isLoggedIn: !!(personal.login.token),
   }
-  return mapStateToProps
 }
 
 const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ fetchAllAlerts }, dispatch)
+  return bindActionCreators({ fetchAllAlerts, getHiddenAlerts }, dispatch)
 }
 
 export class AllAlertsContainer extends Component {
-  componentDidMount () {
+  constructor (props) {
+    super(props)
+    this.checkFullyLoaded = this.checkFullyLoaded.bind(this)
+  }
+
+  checkFullyLoaded () {
     if (this.props.allAlertsStatus === statuses.NOT_FETCHED) {
       this.props.fetchAllAlerts('publish')
     }
+    if (this.props.isLoggedIn && this.props.hiddenAlerts.state === statuses.NOT_FETCHED) {
+      this.props.getHiddenAlerts()
+    }
+  }
+
+  componentDidMount () {
+    this.checkFullyLoaded()
+  }
+
+  componentDidUpdate () {
+    this.checkFullyLoaded()
   }
 
   render () {
-    if (this.props.allAlertsStatus === statuses.SUCCESS) {
-      return <Presenter alerts={this.props.allAlerts} />
+    if (this.props.allAlertsStatus === statuses.SUCCESS && [statuses.SUCCESS, statuses.ERROR].includes(this.props.hiddenAlerts.state)) {
+      return <Presenter alerts={this.props.allAlerts} hiddenIds={typy(this.props.hiddenAlerts, 'data').safeArray} />
     }
     return null
   }
@@ -55,11 +74,17 @@ export class AllAlertsContainer extends Component {
 AllAlertsContainer.propTypes = {
   allAlertsStatus: PropTypes.string,
   fetchAllAlerts: PropTypes.func.isRequired,
+  getHiddenAlerts: PropTypes.func.isRequired,
   allAlerts: PropTypes.object,
+  hiddenAlerts: PropTypes.shape({
+    data: PropTypes.array,
+    state: PropTypes.string.isRequired,
+  }),
+  isLoggedIn: PropTypes.bool.isRequired,
 }
 
 const Alerts = connect(
-  makeMapStateToProps,
+  mapStateToProps,
   mapDispatchToProps
 )(AllAlertsContainer)
 
--- src/components/Contentful/Alert/presenter.js
+++ src/components/Contentful/Alert/presenter.js
@@ -1,23 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+
 import { withErrorBoundary } from 'components/ErrorBoundary'
 import LibMarkdown from 'components/LibMarkdown'
-const Alert = ({ alerts }) => {
+
+import styles from './style.module.css'
+
+const Alert = ({ alerts, hiddenIds }) => {
+  let someHidden = false
   return (
     <div className='alerts'>
       {
         // go through each alert type, putting all similar alerts into the same box
-        Object.keys(alerts).map(key => {
-          const currentAllerts = alerts[key]
-          const className = currentAllerts[0].className
+        Object.keys(alerts).map(type => {
+          const currentAlerts = alerts[type].filter(alert => !hiddenIds || !hiddenIds.includes(alert.id))
+          if (currentAlerts.length < alerts[type].length) {
+            someHidden = true
+            if (!currentAlerts.length) {
+              return null
+            }
+          }
 
+          const className = currentAlerts[0].className
           return (
-            <div className={className} key={key}>
+            <div className={className} key={type}>
               {
-                currentAllerts.map((alert, index) => {
+                currentAlerts.map((alert, index) => {
                   return (
-                    <div className='width' key={key + '_' + alert.url + '_' + index}>
+                    <div className='width' key={type + '_' + alert.url + '_' + index}>
                       <LibMarkdown className='description'>{ alert.description }</LibMarkdown>
+                      <span className={styles.hideAlert}>Hide Alert</span>
                     </div>
                   )
                 })
@@ -26,12 +38,18 @@ const Alert = ({ alerts }) => {
           )
         })
       }
+      { someHidden && (
+        <div className={styles.showHiddenBar}>
+          <span className={styles.showHiddenButton}>Show hidden alerts.</span>
+        </div>
+      )}
     </div>
   )
 }
 
 Alert.propTypes = {
   alerts: PropTypes.object,
+  hiddenIds: PropTypes.arrayOf(PropTypes.string),
 }
 
 export default withErrorBoundary(Alert)
