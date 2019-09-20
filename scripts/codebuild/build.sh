5abf0c5cf337bdc6bc6720ba8da41e5c5a602acc
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2019-09-20 09:33:52 -0400

    A few small tweaks.

--- scripts/codebuild/build.sh
+++ scripts/codebuild/build.sh
@@ -10,5 +10,11 @@ echo -e "\n${blue}Building code...${reset}"
 yarn build --production || { echo "Build failed"; exit 1; }
 
 cd deploy/blueprints
+if [ $LOCAL_DEPLOY = true ]
+then
+  APPROVAL='any-change'
+else
+  APPROVAL='never'
+fi
 echo -e "\n${blue}Deploying with cdk...${reset}"
-cdk deploy $STACK_NAME -c usurperBuildPath='../../build' || { echo "CDK deployment failed"; exit 1; }
+cdk deploy $STACK_NAME -c usurperBuildPath='../../build' --require-approval=$APPROVAL "$@" || { echo "CDK deployment failed"; exit 1; }
--- scripts/codebuild/post_build.sh
+++ scripts/codebuild/post_build.sh
@@ -4,3 +4,11 @@ magenta=`tput setaf 5`
 reset=`tput sgr0`
 
 echo "${magenta}----- POST-BUILD -----${reset}"
+
+# This isn't really necessary in the pipeline so don't waste time bothering unless it's a local deploy
+if [ ${LOCAL_DEPLOY:=false} = true ] && [ $STAGE != 'dev' ]
+then
+  # after deploying, it's kind of annoying and confusing if the configParameters are not set to dev, because your local
+  # build can unexpectedly connect to another environment's microservices. Therefore, rebuld dev config if not already.
+  node ./scripts/build-links/buildConfig.js stage=dev
+fi
