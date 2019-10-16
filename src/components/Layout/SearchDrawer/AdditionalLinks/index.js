161218d0932ea2aa2855caab4ac1649c2bb09a7e
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2019-10-16 10:45:27 -0400

    LW-797 Link to primo directly when clicking advanced search.

--- src/components/Layout/SearchDrawer/AdditionalLinks/index.js
+++ src/components/Layout/SearchDrawer/AdditionalLinks/index.js
@@ -1,37 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { searchOptions } from 'constants/searchOptions'
+import { searchOptions, NDCATALOG } from 'constants/searchOptions'
+import Link from 'components/Interactive/Link'
+import Config from 'shared/Configuration'
 
 const AdditionalLinks = (props) => {
   const links = []
-  let key = 0
   const opt = searchOptions.find((el) => {
     return el.uid === props.currentSearch.uid
   })
   if (opt && opt.enableAdvancedSearch) {
-    links.push( // eslint-disable-next-line jsx-a11y/anchor-is-valid
-      <a
-        onClick={props.toggleAdvancedSearch}
-        onKeyDown={props.toggleAdvancedSearch}
+    let destination = `${Config.onesearchBaseURL}/primo-explore/search?institution=NDU&vid=NDU&mode=advanced`
+    if (props.currentSearch.uid === NDCATALOG) {
+      destination += '&search_scope=nd_campus'
+    }
+
+    links.push(
+      <Link
+        key={destination}
+        to={destination}
         tabIndex='0'
-        key={key++}
         className='advanced-basic-toggle'
-      >{props.advancedButtonLabel}</a>
+      >{props.advancedButtonLabel}</Link>
     )
   }
   if (props.currentSearch.additionalLinks) {
-    if (key > 0) {
-      links.push(<span key={key++}> | </span>)
+    if (links.length > 0) {
+      links.push(<span key='separator'> | </span>)
     }
-    links.push(<span key={key++}>{props.currentSearch.additionalLinks}</span>)
+    links.push(<span key='additional'>{props.currentSearch.additionalLinks}</span>)
   }
 
   return (
-    <div className='additional-links'>{links}</div>)
+    <div className='additional-links'>{links}</div>
+  )
 }
 
 AdditionalLinks.propTypes = {
-  toggleAdvancedSearch: PropTypes.func,
   advancedButtonLabel: PropTypes.string,
   currentSearch: PropTypes.object,
 }
