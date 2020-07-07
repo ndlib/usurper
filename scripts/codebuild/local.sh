3a1cb4c175570149f7042ebec5527581234195d3
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-07-07 09:55:33 -0400

    Update local deploy for convenience.

--- scripts/codebuild/local.sh
+++ scripts/codebuild/local.sh
@@ -11,6 +11,8 @@ export STAGE=${STAGE:="dev"}
 export STACK_NAME=${STACK_NAME:="usurper-${STAGE}"}
 export OWNER=${OWNER:=$(id -un)}
 export CONTACT=${CONTACT:=$OWNER@nd.edu}
+export CREATE_DNS=${CREATE_DNS:="false"}
+export DOMAIN_STACK_NAME=${DOMAIN_STACK_NAME:="library-domain"}
 
 ./scripts/codebuild/install.sh || { exit 1; }
 ./scripts/codebuild/pre_build.sh || { exit 1; }
