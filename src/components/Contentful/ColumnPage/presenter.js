c9538fe2999097bdf8cf9daf9f8e7de17926f195
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-11-12 16:32:30 -0500

    Remove redundancy with navigation model.

--- src/components/Contentful/ColumnPage/presenter.js
+++ src/components/Contentful/ColumnPage/presenter.js
@@ -3,57 +3,12 @@ import PropTypes from 'prop-types'
 import typy from 'typy'
 import SearchProgramaticSet from 'components/SearchProgramaticSet'
 import PageTitle from 'components/Layout/PageTitle'
-import Link from 'components/Interactive/Link'
-import LibMarkdown from 'components/LibMarkdown'
 import SideNav from 'components/Layout/Navigation/SideNav'
 import PageAlert from '../Alert/Page'
 import OpenGraph from 'components/OpenGraph'
-import { getLinkObject } from 'shared/ContentfulLibs'
-import InternalLink from 'components/Contentful/InternalLink'
+import Section from './Section'
 
 import './style.css'
-
-const Sections = (column, showDescriptions) => {
-  return column.fields.items.map((entry) => {
-    const s = entry.fields
-    if (!entry || !entry.fields || !entry.fields.displayName) {
-      return null
-    }
-    return (
-      <section key={entry.sys.id} className='group'>
-        <h2><span id={encodeURIComponent(s.displayName)}>{s.displayName}</span></h2>
-        <LibMarkdown>{ s.body }</LibMarkdown>
-        <div className='linksgroup'>
-          <div role={s.displayName + ' navigation'}>
-            {
-              typy(s.items).safeArray.map((item) => {
-                if (item.sys.contentType.sys.id === 'internalLink') {
-                  return (
-                    <p key={item.sys.id}><InternalLink cfEntry={item} /></p>
-                  )
-                }
-                const linkObject = getLinkObject(item.fields, item.sys.id)
-                return (
-                  <p key={item.sys.id}>
-                    <Link to={linkObject.heading.url} className='item-title'>{linkObject.heading.title}</Link>
-                    <span className='linkGroup'>
-                      {
-                        linkObject.conditionalLinks.map((data) => {
-                          return data.url && <li key={data.keyId}><Link to={data.url}>{data.title}</Link></li>
-                        })
-                      }
-                    </span>
-                    { showDescriptions ? (<span>{linkObject.heading.description}</span>) : null }
-                  </p>
-                )
-              })
-            }
-          </div>
-        </div>
-      </section>
-    )
-  })
-}
 
 const ColumnContainerPresenter = (props) => {
   const page = props.cfPageEntry.fields
@@ -75,31 +30,29 @@ const ColumnContainerPresenter = (props) => {
         <SideNav className='side-nav-bg'>
           <ul>
             {
-              page.items.map((column) => {
-                return column.fields.items.map((section) => {
-                  if (section && section.fields) {
-                    const key = encodeURIComponent(section.fields.displayName)
-                    return (
-                      <a
-                        className='side-anchors'
-                        href={'#' + key}
-                        key={key}>
-                        <li>{section.fields.displayName}</li>
-                      </a>
-                    )
-                  }
-                  return null
-                })
+              page.items.map((section) => {
+                if (section && section.fields) {
+                  const key = encodeURIComponent(section.fields.displayName)
+                  return (
+                    <a
+                      className='side-anchors'
+                      href={'#' + key}
+                      key={key}>
+                      <li>{section.fields.displayName}</li>
+                    </a>
+                  )
+                }
+                return null
               })
             }
           </ul>
         </SideNav>
         <div className='row landing'>
           {
-            page.items.map((column, index) => {
+            page.items.map((section, index) => {
               return (
-                <div className='col-md-12 col-xs-12' key={'column_' + index}>
-                  { Sections(column, typy(page, 'extraData.showDescriptions').safeBoolean) }
+                <div className='col-md-12 col-xs-12' key={'section_' + index}>
+                  <Section entry={section} showDescriptions={typy(page, 'extraData.showDescriptions').safeBoolean} />
                 </div>
               )
             })
