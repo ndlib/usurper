8c471df93bf272f78bc6a6a0e6a0f2a57a04dcd2
Michael Kirkpatrick <mkirkpatrick@nd.edu>
2020-12-18 11:42:35 -0500

    Add service worker.
    Had to move some css because of code splitting.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,47 +1,47 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Route, Switch, withRouter } from 'react-router'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { withCookies } from 'react-cookie'
 import typy from 'typy'
+import rootReducers from 'reducers'
+import thunkMiddleware from 'redux-thunk'
+
+import { SET_SEARCH, SAVE_SEARCH_PREFERENCE } from 'actions/search'
 
 import Config from 'shared/Configuration'
 import PageWrapper from 'components/Layout/PageWrapper'
-import Home from 'components/Home'
-import ItemsRequests from 'components/Account/ItemsRequests'
-import Courses from 'components/Account/Courses'
-import Hours from 'components/Hours/Page'
-import ChatPage from 'components/ChatPage'
-import ContentfulPage from 'components/Contentful/Page'
-import ContentfulColumnPage from 'components/Contentful/ColumnPage'
-import SecureContentfulPage from 'components/Contentful/SecurePage'
-import ContentfulFloor from 'components/Contentful/Floor'
-import ContentfulNews from 'components/Contentful/News'
-import DatabasePage from 'components/Contentful/Database'
-import SearchPage from 'components/SearchPage'
-import ContentfulEvent from 'components/Contentful/Event'
-import News from 'components/LandingPages/News'
-// import StudySpaces from 'components/LandingPages/StudySpaces'
-import CurrentEvents from 'components/LandingPages/Events/Current'
-import PastEvents from 'components/LandingPages/Events/Past'
-import CurrentExhibits from 'components/LandingPages/Exhibits/Current'
-import PastExhibits from 'components/LandingPages/Exhibits/Past'
-import DatabaseList from 'components/DatabaseList'
-import SubjectList from 'components/SubjectList'
-import FloorSearch from 'components/FloorSearch'
-import rootReducers from 'reducers'
-import thunkMiddleware from 'redux-thunk'
-import CirculationHistory from 'components/Account/CirculationHistory'
-import Preferences from 'components/Account/Preferences'
-import Reservations from 'components/Account/Reservations'
-import Floors from 'components/DynamicPages/FloorList'
-import RoomReservations from 'components/DynamicPages/RoomReservations'
-// import StudySpaces from 'components/LandingPages/StudySpaces'
-
+import Loading from 'components/Messages/Loading'
 import NotFound from 'components/Messages/NotFound'
-
 import { LINK_CLICK } from 'components/Interactive/Link'
-import { SET_SEARCH, SAVE_SEARCH_PREFERENCE } from 'actions/search.js'
+
+import Home from 'components/Home'
+const ItemsRequests = lazy(() => import('components/Account/ItemsRequests'))
+const Courses = lazy(() => import('components/Account/Courses'))
+const Hours = lazy(() => import('components/Hours/Page'))
+const ChatPage = lazy(() => import('components/ChatPage'))
+const ContentfulPage = lazy(() => import('components/Contentful/Page'))
+const ContentfulColumnPage = lazy(() => import('components/Contentful/ColumnPage'))
+const SecureContentfulPage = lazy(() => import('components/Contentful/SecurePage'))
+const ContentfulFloor = lazy(() => import('components/Contentful/Floor'))
+const ContentfulNews = lazy(() => import('components/Contentful/News'))
+const DatabasePage = lazy(() => import('components/Contentful/Database'))
+const SearchPage = lazy(() => import('components/SearchPage'))
+const ContentfulEvent = lazy(() => import('components/Contentful/Event'))
+const News = lazy(() => import('components/LandingPages/News'))
+// const StudySpaces = lazy(() => import('components/LandingPages/StudySpaces'))
+const CurrentEvents = lazy(() => import('components/LandingPages/Events/Current'))
+const PastEvents = lazy(() => import('components/LandingPages/Events/Past'))
+const CurrentExhibits = lazy(() => import('components/LandingPages/Exhibits/Current'))
+const PastExhibits = lazy(() => import('components/LandingPages/Exhibits/Past'))
+const DatabaseList = lazy(() => import('components/DatabaseList'))
+const SubjectList = lazy(() => import('components/SubjectList'))
+const FloorSearch = lazy(() => import('components/FloorSearch'))
+const CirculationHistory = lazy(() => import('components/Account/CirculationHistory'))
+const Preferences = lazy(() => import('components/Account/Preferences'))
+const Reservations = lazy(() => import('components/Account/Reservations'))
+const Floors = lazy(() => import('components/DynamicPages/FloorList'))
+const RoomReservations = lazy(() => import('components/DynamicPages/RoomReservations'))
 
 const analyticsActions = [LINK_CLICK, SET_SEARCH, SAVE_SEARCH_PREFERENCE]
 
@@ -71,49 +71,51 @@ const App = (props) => {
     <Switch>
       <Provider store={store}>
         <PageWrapper {...props}>
-          <Switch>
-            <Route exact path='/' render={() => (<Home {...props} />)} /> {/* Render is needed to pass cookies */}
-            <Route exact path='/chat' component={ChatPage} />
-            <Route exact path='/courses' component={Courses} />
-            <Route exact path='/hours' component={Hours} />
-            <Route exact path='/events' component={CurrentEvents} />
-            <Route exact path='/events/past' component={PastEvents} />
-            <Route exact path='/events/past/:date' component={PastEvents} />
-            <Route exact path='/events/:date' component={CurrentEvents} />
-            { Config.features.exhibitsEnabled && (
-              <Route exact path='/exhibits' component={CurrentExhibits} />
-            )}
-            { Config.features.exhibitsEnabled && (
-              <Route exact path='/exhibits/past' component={PastExhibits} />
-            )}
-            { Config.features.exhibitsEnabled && (
-              <Route exact path='/exhibits/past/:date' component={PastExhibits} />
-            )}
-            { Config.features.exhibitsEnabled && (
-              <Route exact path='/exhibits/:date' component={CurrentExhibits} />
-            )}
-            <Route exact path='/news' component={News} />
-            <Route exact path='/floor/search' component={FloorSearch} />
-            <Route exact path='/floor/:id' component={ContentfulFloor} />
-            <Route exact path='/hesburgh-floor-maps' render={() => (<Floors {...props} title='Hesburgh Library Floor Maps' slug='hesburgh-library-building' />)} />
-            <Route exact path='/news/:id' component={ContentfulNews} />
-            <Route exact path='/event/:id' component={ContentfulEvent} />
-            <Route exact path='/items-requests' component={ItemsRequests} />
-            <Route exact path='/checkout-history' component={CirculationHistory} />
-            <Route exact path='/preferences' render={() => (<Preferences {...props} />)} />
-            <Route exact path='/reservations/:dateRange?' component={Reservations} />
-            <Route exact path='/subjects' component={SubjectList} />
-            <Route exact path='/database/:id' component={DatabasePage} />
-            <Route exact path='/databases' component={DatabaseList} />
-            <Route exact path='/room-reservations' component={RoomReservations} />
-            { /* <Route exact path='/study-spaces' component={StudySpaces} /> */ }
-            <Route exact path='/secure/:id' component={SecureContentfulPage} />
-            <Route exact path='/(services|research|libraries|about)' component={ContentfulColumnPage} />
-            <Route exact path='/search' component={SearchPage} />
-            <Route exact path='/:id' component={ContentfulPage} />
+          <Suspense fallback={<Loading />}>
+            <Switch>
+              <Route exact path='/' render={() => (<Home {...props} />)} /> {/* Render is needed to pass cookies */}
+              <Route exact path='/chat' component={ChatPage} />
+              <Route exact path='/courses' component={Courses} />
+              <Route exact path='/hours' component={Hours} />
+              <Route exact path='/events' component={CurrentEvents} />
+              <Route exact path='/events/past' component={PastEvents} />
+              <Route exact path='/events/past/:date' component={PastEvents} />
+              <Route exact path='/events/:date' component={CurrentEvents} />
+              { Config.features.exhibitsEnabled && (
+                <Route exact path='/exhibits' component={CurrentExhibits} />
+              )}
+              { Config.features.exhibitsEnabled && (
+                <Route exact path='/exhibits/past' component={PastExhibits} />
+              )}
+              { Config.features.exhibitsEnabled && (
+                <Route exact path='/exhibits/past/:date' component={PastExhibits} />
+              )}
+              { Config.features.exhibitsEnabled && (
+                <Route exact path='/exhibits/:date' component={CurrentExhibits} />
+              )}
+              <Route exact path='/news' component={News} />
+              <Route exact path='/floor/search' component={FloorSearch} />
+              <Route exact path='/floor/:id' component={ContentfulFloor} />
+              <Route exact path='/hesburgh-floor-maps' render={() => (<Floors {...props} title='Hesburgh Library Floor Maps' slug='hesburgh-library-building' />)} />
+              <Route exact path='/news/:id' component={ContentfulNews} />
+              <Route exact path='/event/:id' component={ContentfulEvent} />
+              <Route exact path='/items-requests' component={ItemsRequests} />
+              <Route exact path='/checkout-history' component={CirculationHistory} />
+              <Route exact path='/preferences' render={() => (<Preferences {...props} />)} />
+              <Route exact path='/reservations/:dateRange?' component={Reservations} />
+              <Route exact path='/subjects' component={SubjectList} />
+              <Route exact path='/database/:id' component={DatabasePage} />
+              <Route exact path='/databases' component={DatabaseList} />
+              <Route exact path='/room-reservations' component={RoomReservations} />
+              { /* <Route exact path='/study-spaces' component={StudySpaces} /> */ }
+              <Route exact path='/secure/:id' component={SecureContentfulPage} />
+              <Route exact path='/(services|research|libraries|about)' component={ContentfulColumnPage} />
+              <Route exact path='/search' component={SearchPage} />
+              <Route exact path='/:id' component={ContentfulPage} />
 
-            <Route path='*' component={NotFound} />
-          </Switch>
+              <Route path='*' component={NotFound} />
+            </Switch>
+          </Suspense>
         </PageWrapper>
       </Provider>
     </Switch>
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import * as Sentry from '@sentry/browser'
 import App from './components/App'
 import Config from 'shared/Configuration'
+import { Workbox } from 'workbox-window'
 
 Sentry.init({
   dsn: 'https://1df50f8918594d20b7cf9c6464238e0d@sentry.io/1281163',
@@ -18,3 +19,15 @@ render((
   </BrowserRouter>),
 document.getElementById('root')
 )
+
+if ('serviceWorker' in navigator) {
+  const wb = new Workbox('service-worker.js')
+
+  wb.addEventListener('installed', (event) => {
+    if (event.isUpdate) {
+      window.location.reload()
+    }
+  })
+
+  wb.register()
+}
