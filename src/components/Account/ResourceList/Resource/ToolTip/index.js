aeef9d383eac7b1b29ddec4b635eed863d8c883e
Paul Weikel <pweikel@nd.edu>
2021-03-12 13:07:10 -0500

    added missing period

--- src/components/Account/ResourceList/Resource/ToolTip/index.js
+++ src/components/Account/ResourceList/Resource/ToolTip/index.js
@@ -5,7 +5,7 @@ import styles from './style.module.css'
 
 const ToolTip = (props) => {
   const value = props.value ? 'Yes' : 'No'
-  const tip = (props.value === 'No' ? 'Renewal not available. Please return by due date.' : 'Please click the "view in ILL" button to renew')
+  const tip = (props.value === 'No' ? 'Renewal not available. Please return by due date.' : 'Please click the "view in ILL" button to renew.')
   return (
     <div aria-labelledby={props.title} className={styles.tooltip}>{value}
       <span role='tooltip' id={props.title} className={styles.tooltiptext}>{tip}</span>
