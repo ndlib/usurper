2408b822294fdf2ca1474ffa561221d99c7aa9f6
Paul Weikel <pweikel@nd.edu>
2021-03-12 12:24:50 -0500

    added accessibility tags

--- src/components/Account/ResourceList/Resource/ToolTip/index.js
+++ src/components/Account/ResourceList/Resource/ToolTip/index.js
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types'
 import styles from './style.module.css'
 
 const ToolTip = (props) => {
-  const placeValue = props.value ? 'Yes' : 'No'
-  const toolTip = (placeValue === 'No' ? 'Renewal not available. Please return by due date.' : 'Please click the "view in ILL" button to renew.')
+  const value = props.value ? 'Yes' : 'No'
+  const tip = (props.value === 'No' ? 'Renewal not available. Please return by due date.' : 'Please click the "view in ILL" button to renew')
   return (
-    <div className={styles.tooltip}>{placeValue}
-      <span className={styles.tooltiptext}>{toolTip}</span>
+    <div aria-labelledby='tip' className={styles.tooltip}>{value}
+      <span id='tip' className={styles.tooltiptext}>{tip}</span>
     </div>
   )
 }
